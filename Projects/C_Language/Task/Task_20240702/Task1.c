8423c390d4160f019605a68661588703b0415a31
Kinesis <lordkinesis@naver.com>
2024-07-02 13:57:17 +0900

    Update: Task1.c
    종류1 구현 완료.

--- Projects/C_Language/Task/Task_20240702/Task1.c
+++ Projects/C_Language/Task/Task_20240702/Task1.c
@@ -1,9 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void DrawingStar_Type1(int sizeGuide);
+//void DrawingStar_Type2(int sizeGuide);
+//void DrawingStar_Type3(int sizeGuide);
+//void DrawingStar_Type4(int sizeGuide);
+
 int main() {
 
+	// 변수 선언하기
+	int size, type;
 
+	printf("사이즈와 종류를 입력하시오.");
+	// 선배님께서 말씀하신 입력 조건 : size = 홀수만, max : 9
+	scanf("%d %d", &size, &type);
+
+	switch (type)
+	{
+	case 1:
+		DrawingStar_Type1(size);
+		break;
+	/*case 2:
+		DrawingStar_Type2(size);
+		break;
+	case 3:
+		DrawingStar_Type3(size);
+		break;
+	case 4:
+		DrawingStar_Type4(size);
+		break;*/
+	default:
+		break;
+	}
 
 	return 0;
+}
+
+// 종류 1 출력하는 알고리즘
+void DrawingStar_Type1(int sizeGuide) {
+
+	int i, j;
+	for (i = 0; i < sizeGuide; i++) {
+		for (j = 0; j <=i; j++) {
+			if (j <= i && j < sizeGuide - i) {
+				printf("*");
+			}else if(j < sizeGuide - i){
+				printf("*");
+			}else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 }
