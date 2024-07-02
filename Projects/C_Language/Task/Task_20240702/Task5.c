acf175f33cbad5bcac7be44b2bec6f78354b54a0
Kinesis <lordkinesis@naver.com>
2024-07-02 22:40:59 +0900

    Update: Task5.c
    '문자' 찾기 구현 완료.
    TODO:
    1. 중복 문자의 위치 찾기
    2. 문자열 위치 찾기

--- Projects/C_Language/Task/Task_20240702/Task5.c
+++ Projects/C_Language/Task/Task_20240702/Task5.c
@@ -7,7 +7,7 @@ int main() {
 	char inputString[100] = " ";
 	char searchString[100] = " ";
 	int rememberStrPosition[100] = { 0, };
-	int i;
+	int i, j, searchIndex = 0;
 
 	// 형식 출력하기
 	printf("최대 99개 문자 입력 <inputStr> : ");
@@ -22,9 +22,25 @@ int main() {
 		/*if (inputString[i] == '\0') {
 			break;
 		}*/
-		printf("%c", inputString[i]);
+
+		//  한 글자 분석
+		if (inputString[i] == searchString[searchIndex]) {
+			/*printf("%c", inputString[i]);*/
+			// 출력은 1부터 이므로, 출력을 원할하게 하기 위해 i + 1으로 구성함.
+			rememberStrPosition[searchIndex] = i + 1;
+			searchIndex++;
+		}else if(searchString[searchIndex] == '\0'){
+			break;
+		}
 	}
 
+	printf("%s의 위치 : ", searchString);
+	for (i = 0; i < 100; i++) {
+		if (rememberStrPosition[i] == 0) {
+			break;
+		}
+		printf("%d", rememberStrPosition[i]);
+	}
 
 	return 0;
 }
