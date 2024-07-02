5d6aa2a02f38380a4e02c0040ba716188e9a8325
Kinesis <lordkinesis@naver.com>
2024-07-02 16:28:00 +0900

    Update: Task3.c
    완료.

--- Projects/C_Language/Task/Task_20240702/Task3.c
+++ Projects/C_Language/Task/Task_20240702/Task3.c
@@ -1,2 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+int CheckingScale(char inputString[8]);
+
+int main() {
+
+	// 변수 선언하기
+	char userInput[8] = { ' ', };
+	int i;
+
+	// 형식 출력하기
+	printf("음 입력\n");
+	for (i = 0; i < 8; i++) {
+		scanf("%c", &userInput[i]);
+	}
+	
+	switch (CheckingScale(userInput))
+	{
+	case 1:
+		printf("결과 : ascending\n");
+		break;
+	case 2:
+		printf("결과 : descending\n");
+		break;
+	case 3:
+		printf("결과 : mixed\n");
+		break;
+	default:
+		break;
+	}	
+
+	return 0;
+}
+
+int CheckingScale(char inputString[8]) {
+	// 변수 선언하기
+	char stringAscending[8] = "cdefgabC";
+	char stringDescending[8] = "Cbagfedc";
+	int i, j, cntScale1 = 0, cntScale2 = 0; // cntScale : Scle 체크 변수
+
+	for (i = 0; i < 8; i++) {
+		if (inputString[i] == stringAscending[i]) {
+			cntScale1++;
+		}else if(inputString[i] == stringDescending[i]){
+			cntScale2++;
+		}
+	}
+
+	if (cntScale1 == 8) {
+		return 1;
+	}else if (cntScale2 == 8) {
+		return 2;
+	}else{
+		return 3;
+	}
+}
