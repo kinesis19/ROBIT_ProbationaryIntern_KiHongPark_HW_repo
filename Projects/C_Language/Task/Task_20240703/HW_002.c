dee7d62d62f0f12ef6df3b60932c1750fdea0b22
Kinesis <lordkinesis@naver.com>
2024-07-03 15:14:43 +0900

    Create: HW_002.c
    과제 2부터 해결해 보자.

---
+++ Projects/C_Language/Task/Task_20240703/HW_002.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	// 변수 선언하기
+	int min = 0, max = 0;
+	int numAry[10000] = { 0, };
+
+	// 형식 출력하기
+	printf("min : ");
+	scanf("%d", &min);
+	printf("max : ");
+	scanf("%d", &max);
+
+	// 알고리즘 구현하기
+
+
+
+
+	return 0;
+}
