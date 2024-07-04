d3704ab421adf4896ba514bd44d004402a679d1e
Kinesis <lordkinesis@naver.com>
2024-07-04 10:50:19 +0900

    Update: HW_001.c
    동적할당 배열 선언 -> num1부터 ~ num2까지의 출력 성공

--- Projects/C_Language/HomeWork/HW_20240704/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240704/HW_001.c
@@ -1,22 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-	// 변수 선언하기
+	// 변수 선언하기.
 	int num1, num2, i, j, cnt = 0;
 
-	// 형식 입출력 하기
+	// 형식 입출력 하기.
 	printf("입력 : ");
 	scanf("%d %d", &num1, &num2);
+	int* pArr;
 
-	
+	/* 동적 할당하기.
+	* 이중 포인터 X -> 이중 포인터 개념은 배우지 않았기 때문임.
+	* int type의 2차원 배열 생성 -> 해당 값들을 동적 배열 (포인터 배열)에 할당하기
+	*/
+	pArr = (int*)malloc(sizeof(int) * (num1 * num2));
+
+	/*if (pArr == NULL) {
+		printf("malloc error!");
+	}*/
+
 	for (i = 0; i < num1; i++) {
 		for (j = 0; j < num2; j++) {
+			pArr[cnt] = cnt + 1;
+			printf("%d ", pArr[cnt]);
 			cnt++;
-			printf("%d ", cnt);
 		}
 		printf("\n");
 	}
+
+	free(pArr);
 
 }
