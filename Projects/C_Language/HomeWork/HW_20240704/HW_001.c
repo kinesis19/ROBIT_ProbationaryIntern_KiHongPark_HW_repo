d93e828a3528842c9d0b99adafa13d91e5e7f352
Kinesis <lordkinesis@naver.com>
2024-07-04 10:36:13 +0900

    Update: HW_001.c

--- Projects/C_Language/HomeWork/HW_20240704/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240704/HW_001.c
@@ -3,10 +3,20 @@
 
 int main() {
 
-	int num1, num2;
+	// 변수 선언하기
+	int num1, num2, i, j, cnt = 0;
 
+	// 형식 입출력 하기
 	printf("입력 : ");
 	scanf("%d %d", &num1, &num2);
 
+	
+	for (i = 0; i < num1; i++) {
+		for (j = 0; j < num2; j++) {
+			cnt++;
+			printf("%d ", cnt);
+		}
+		printf("\n");
+	}
 
 }
