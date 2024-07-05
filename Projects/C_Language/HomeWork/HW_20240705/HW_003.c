c7a7b0cd202023b8f51073fc5478e032cf68f826
Kinesis <lordkinesis@naver.com>
2024-07-05 17:52:05 +0900

    Create: HW_003.c
    임시 저장.
    동적 배열 생성 완료.
    그러나 동적 배열에 이상한 값(기존 값)들이 겹쳐서 들어가는 상황이 발생됨.

---
+++ Projects/C_Language/HomeWork/HW_20240705/HW_003.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _something {
+
+	char name[20];
+	int cost;
+}Something;
+
+
+int main() {
+
+	// 변수 선언하기.
+	int inputNum, i, wantIdx = 1;
+	// 형식 입출력하기.
+	printf("입력 : ");
+	scanf("%d", &inputNum);
+
+	char* stuffName = (char*)calloc(inputNum, sizeof(char) * 20);
+	int* stuffCost = (int*)calloc(inputNum, sizeof(int));
+
+
+	for (i = 0; i < inputNum; i++) {
+		scanf("%s %d", &stuffName[i], &stuffCost[i]);
+		wantIdx++;
+	}
+
+
+	scanf("%s", &stuffName[wantIdx]);
+
+	printf("출력 : ");
+
+	printf("%s \n\n", &stuffName[wantIdx]);
+
+
+	//// 찾는 물건이 입력된 물건 리스트에 있는지 찾아보는 알고리즘.
+	//for (i = 0; i < inputNum; i++) {
+	//	// 찾는 물건이 리스트에 있을 때,
+	//	if (&stuffName[i] == &stuffName[wantIdx]) {
+	//		printf("있음");
+	//	}
+	//	printf("%d ", &stuffName[i] == &stuffName[wantIdx]);
+	//}
+	//// 없다면 0을 출력하고 종료함.
+	//printf("0");
+	//return 0;
+
+
+
+	for (i = 0; i < inputNum; i++) {
+		printf("%s %d\n", &stuffName[i], stuffCost[i]);
+	}
+
+
+	return 0;
+}
