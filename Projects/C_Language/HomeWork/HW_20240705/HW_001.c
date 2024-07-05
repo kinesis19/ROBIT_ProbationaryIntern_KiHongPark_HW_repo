ec6a32e2545dc86a1c54ef8787681f0592ad5c12
Kinesis <lordkinesis@naver.com>
2024-07-05 11:11:01 +0900

    Update: HW_001.c
    구조체 선언, 호출, 값 할당까지 구현 완료.
    TODO:
    - 정렬 규칙 시스템을 만들어 반영하기.

--- Projects/C_Language/HomeWork/HW_20240705/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240705/HW_001.c
@@ -1,7 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+// 학년 구조체 선언
+typedef struct _Student {
+
+	int schoolYear;
+	double grade;
+	char name;
+}Student;
+
 int main() {
+
+	// 변수 선언하기.
+	int personMax = 5;
+	int i;
+
+	// 구조체 정의하기
+	Student student[5];
+
+
+	// 형식 출력하기.
+	printf("입력 : ");
+
+	for (i = 0; i < personMax; i++) {
+		if (i != 0) {
+			printf("       ");
+		}
+		scanf("%d %lf %s", &student[i].schoolYear, &student[i].grade, &student[i].name);
+	}
+
+	printf("\n출력 : ");
+	for (i = 0; i < personMax; i++) {
+		if (i != 0) {
+			printf("       ");
+		}
+		printf("%d %.1lf %s\n", student[i].schoolYear, student[i].grade, &student[i].name);
+	}
+
 
 	return 0;
 }
