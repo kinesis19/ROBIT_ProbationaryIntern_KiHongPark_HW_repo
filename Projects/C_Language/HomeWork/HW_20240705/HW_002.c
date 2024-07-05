5640959965926addb49d1bac424871eef7517e59
Kinesis <lordkinesis@naver.com>
2024-07-05 17:15:23 +0900

    Update: HW_002.c
    임시 저장.
    동적 배열로 저장하는 것 까지는 완료함.
    이 값들을 어떻게 구조체에 저장할 수 있는지 모르겠음. 더 생각해 봐야할 듯.

--- Projects/C_Language/HomeWork/HW_20240705/HW_002.c
+++ Projects/C_Language/HomeWork/HW_20240705/HW_002.c
@@ -14,23 +14,30 @@ int main() {
 	// 변수 생성하기.
 	int inputNum = 0;
 	int i;
-	int* pArrPosX = (int*)calloc(inputNum, sizeof(int));
-	int* pArrPosY = (int*)calloc(inputNum, sizeof(int));
 
+	// 형식 입출력받기.
 	printf("입력 : ");
 	scanf("%d", &inputNum);
+
+	int* pArrPosX = (int*)calloc(inputNum, sizeof(int));
+	int* pArrPosY = (int*)calloc(inputNum, sizeof(int));
 
 	for (i = 0; i < inputNum; i++) {
 		printf("       ");
 		scanf("%d %d", &pArrPosX[i], &pArrPosY[i]);
 	}
-
-	printf("\n");
+	
+	for (i = 0; i < inputNum; i++) {
+		Position position = { pArrPosX[i], pArrPosY[i] };
+	}
 
 	for (i = 0; i < inputNum; i++) {
-		printf("       ");
-		printf("%d %d\n", pArrPosX[i], pArrPosY[i]);
+
 	}
+
+	printf("\n출력 : 가장 거리가 먼 좌표는 (%d, %d)이며, 다른 좌표의 거리 총합은 약 %d입니다.", 1, 1, 1);
+
+
 
 
 	return 0;
