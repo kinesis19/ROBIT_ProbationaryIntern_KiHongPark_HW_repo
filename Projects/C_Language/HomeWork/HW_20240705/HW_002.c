71842a352401b559d5c3131c4ef3bc67f60eba89
Kinesis <lordkinesis@naver.com>
2024-07-05 16:41:40 +0900

    Update: HW_002.c
    동적할당 기능 구현 완료.

--- Projects/C_Language/HomeWork/HW_20240705/HW_002.c
+++ Projects/C_Language/HomeWork/HW_20240705/HW_002.c
@@ -1,8 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _position {
+
+	int posX;
+	int posY;
+}Position;
+
 
 int main() {
 
+	// 변수 생성하기.
+	int inputNum = 0;
+	int i;
+	int* pArrPosX = (int*)calloc(inputNum, sizeof(int));
+	int* pArrPosY = (int*)calloc(inputNum, sizeof(int));
+
+	printf("입력 : ");
+	scanf("%d", &inputNum);
+
+	for (i = 0; i < inputNum; i++) {
+		printf("       ");
+		scanf("%d %d", &pArrPosX[i], &pArrPosY[i]);
+	}
+
+	printf("\n");
+
+	for (i = 0; i < inputNum; i++) {
+		printf("       ");
+		printf("%d %d\n", pArrPosX[i], pArrPosY[i]);
+	}
 
 
 	return 0;
