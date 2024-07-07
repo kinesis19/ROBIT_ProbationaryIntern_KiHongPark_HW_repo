8f49562b70100b18f14bd95ed3b93f4f6c6a07d3
Kinesis <lordkinesis@naver.com>
2024-07-07 22:23:45 +0900

    Update: HW_003.c
    최종 구현 완료.
    테스트 완료.
    
    string 헤더파일을 3차시에 배웠었음. strcmp() 함수가 있는데, 이 함수를 사용하면 쉽게 알고리즘을 설계할 수가 있음. 이런.. 배웠던 내용을 조금만 더 빠르게 떠올릴 걸..

--- Projects/C_Language/HomeWork/HW_20240705/HW_003.c
+++ Projects/C_Language/HomeWork/HW_20240705/HW_003.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> //C언어 3일차 교육에서 배운 헤더파일.
 
 typedef struct _something {
 
@@ -17,40 +18,33 @@ int main() {
 	printf("입력 : ");
 	scanf("%d", &inputNum);
 
+	// 구조체에 동적할당하기.
 	Something* something = calloc(inputNum, sizeof(char) * 20);
 
-
+	// 동적할당한 값을 입력 받아 구조체에 대입하기.
 	for (i = 0; i < inputNum; i++) {
 		printf("       ");
 		scanf("%s %d", &something[i].name, &something[i].cost);
 		wantIdx++;
 	}
 
-
 	printf("       ");
 	scanf("%s", &something[i].name);
 
 	printf("\n출력 : ");
 
-
-	// Debugging:
-	for (i = 0; i < inputNum; i++) {
-		// 찾는 물건이 리스트에 있을 때,
-		printf("%s %s : ", &something[i].name, &something[inputNum].name);
-		printf("%d \n", something[i].name == something[inputNum].name);
-	}
-
 	int totalCost = 0;
-	//// 찾는 물건이 입력된 물건 리스트에 있는지 찾아보는 알고리즘.
+	// 찾는 물건이 입력된 물건 리스트에 있는지 찾아보는 알고리즘.
 	for (i = 0; i < inputNum; i++) {
 		// 찾는 물건이 리스트에 있을 때,
-		if (&something[i].name == &something[wantIdx].name) {
-			printf("aaaaaaaa");
-			printf("%d ", something[i].cost);
+		if (strcmp(something[i].name, something[inputNum].name) == 0) {
 			totalCost = totalCost + something[i].cost;
 		}
 	}
 	printf("%d", totalCost);
+	// 동적할당 해제하기.
+	free(something);
+
 	return 0;
 
 }
