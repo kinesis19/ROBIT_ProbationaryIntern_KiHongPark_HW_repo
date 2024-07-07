24f323df1c12e417695c42bbb033511fd4491da2
Kinesis <lordkinesis@naver.com>
2024-07-07 17:33:09 +0900

    Update: HW_002.c
    임시 저장.
    동적할당된 값을 구조체에 대입하는 것까지 구현 완료.

--- Projects/C_Language/HomeWork/HW_20240705/HW_002.c
+++ Projects/C_Language/HomeWork/HW_20240705/HW_002.c
@@ -6,6 +6,7 @@ typedef struct _position {
 
 	int posX;
 	int posY;
+	int posTotal; // 가장 거리가 먼 좌표를 저장하기 위한 변수
 }Position;
 
 
@@ -22,23 +23,41 @@ int main() {
 	int* pArrPosX = (int*)calloc(inputNum, sizeof(int));
 	int* pArrPosY = (int*)calloc(inputNum, sizeof(int));
 
+	// 포인터 개념을 사용하여 동적할당된 값을 구조체에 저장하기 위한 선언.
+	Position* position = (int*)malloc(inputNum, sizeof(int));
+
+
 	for (i = 0; i < inputNum; i++) {
 		printf("       ");
 		scanf("%d %d", &pArrPosX[i], &pArrPosY[i]);
 	}
+
+	// 동적할당된 값을 구조체에 대입하기.
+	for (i = 0; i < inputNum; i++) {
+		position[i].posX = pArrPosX[i];
+		position[i].posY = pArrPosY[i];
+		position[i].posTotal = (pArrPosX[i] * pArrPosX[i]) + (pArrPosY[i] * pArrPosY[i]);
+	}
+
+	// 입력된 좌표 중 가장 멀리 있는 좌표를 찾아내기.
 	
+	/*for (i = 0; i < inputNum; i++) {
+		for (int j = 0; j < inputNum; j++) {
+
+		}
+	}*/
+
+	
+
+	// Debugging: 동적할당된 값이 구조체에 잘 저장되었는지 출력하기.
 	for (i = 0; i < inputNum; i++) {
-		Position position = { pArrPosX[i], pArrPosY[i] };
+		printf("%d %d %d\n", position[i].posX, position[i].posY, position[i].posTotal);
 	}
 
-	for (i = 0; i < inputNum; i++) {
-
-	}
-
+	// Debugging: Number는 임시로 출력.
 	printf("\n출력 : 가장 거리가 먼 좌표는 (%d, %d)이며, 다른 좌표의 거리 총합은 약 %d입니다.", 1, 1, 1);
 
-
-
+	free(position);
 
 	return 0;
 }
