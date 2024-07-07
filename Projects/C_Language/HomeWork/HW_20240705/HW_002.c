cd5b4170a2d1e1cc148094bb797b5e7267d0ccd9
Kinesis <lordkinesis@naver.com>
2024-07-07 19:14:21 +0900

    Update: HW_002.c
    동일한 두 수의 제곱근 구하는 것은 완료함. 동일한 두 수가 아닐 때의 제곱근(실수)를 구하는 시스템을 구현해야 함. 근데 모르겠음. 우선 다른 과제부터 진행하기로 함.

--- Projects/C_Language/HomeWork/HW_20240705/HW_002.c
+++ Projects/C_Language/HomeWork/HW_20240705/HW_002.c
@@ -42,25 +42,44 @@ int main() {
 	// 입력된 좌표 중 가장 멀리 있는 좌표를 찾아내기.
 	int highPosX = position[0].posX;
 	int highPosY = position[0].posY;
+	int highPosTot = position[0].posTotal;
 	for (i = 0; i < inputNum; i++) {
 		for (int j = 0; j < inputNum; j++) {
-			if (position[i].posTotal < position[j].posTotal) {
+			if (highPosTot < position[j].posTotal) {
 				highPosX = position[j].posX;
 				highPosY = position[j].posY;
+				highPosTot = position[j].posTotal;
 			}
 		}
 	}
 
-	
+	// 다른 좌표의 거리 총합 구하기.
+	for (i = 0; i < inputNum; i++) {
+		printf("%d %d\n\n", position[i].posTotal, highPosTot);
+		if (position[i].posTotal != highPosTot) {
+			// 두 좌표의 직선 길이 구하기. (루트 값에 제곱근 제거한 상태)
+			printf("highPosX:%d, position[%d].posX: %d\nhightPosY: %d, position[%d].posY: %d\n\n", highPosX, i, position[i].posX, highPosY, i, position[i].posY);
+			position[i].posTotal = ((highPosX - position[i].posX) * (highPosX - position[i].posX) + (highPosY - position[i].posY) * (highPosY - position[i].posY));
+		}
+	}
+	// Debugging: 제곱근 구하기 (동일한 두 수의 곲)
+	for (i = 0; i < inputNum; i++) {
+		for (int j = 0; j < inputNum; j++) {
+			if (j * j == position[i].posTotal) {
+				printf("가장 거리가 먼 좌표와 position[%d].posTotal의 거리에 대한 제곱근은 %d입니다.\n", i, j);
+			}
+		}
+	}
+
+
 
 	// Debugging: 동적할당된 값이 구조체에 잘 저장되었는지 출력하기.
 	for (i = 0; i < inputNum; i++) {
-		/*printf("%d %d\n", position[i].posX, position[i].posY);*/
 		printf("%d %d %d\n", position[i].posX, position[i].posY, position[i].posTotal);
 	}
 
 	// Debugging: Number는 임시로 출력.
-	printf("\n출력 : 가장 거리가 먼 좌표는 (%d, %d)이며, 다른 좌표의 거리 총합은 약 %d입니다.", highPosX, highPosY, 1);
+	printf("\n출력 : 가장 거리가 먼 좌표는 (%d, %d)이며, 다른 좌표의 거리 총합은 약 %.1lf입니다.", highPosX, highPosY, 1.0);
 
 	return 0;
 }
