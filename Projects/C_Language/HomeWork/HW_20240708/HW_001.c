c472d7a3e3659c819b411da0db54a7de119ba8e0
Kinesis <lordkinesis@naver.com>
2024-07-08 11:54:02 +0900

    Update: HW_001.c
    임시 저장.
    구현은 완료.
    테스트할 필요가 있음.

--- Projects/C_Language/HomeWork/HW_20240708/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240708/HW_001.c
@@ -4,25 +4,61 @@
 int main() {
 
 	// 변수 선언하기.
-	char inputText[20] = { '\0', }	;
-	int i, textCnt = 0;
+	char inputText[30] = { '\0', };
+	char* pText[30];
+	char tempChar, mostChar; // 최다 등장 비교용도.
+	int cmpCnt = 0, tempCmpCnt = 0;
+	int i, j, textCnt = 0, textSpaceCnt = 0;
+
+	*pText = &inputText;
 
 	// 입출력 받기.
 	printf("입력 : ");
 	scanf("%[^\n]s", &inputText); // 공백으로 입력 받기 위한 연산자 (광운대 SW예비학교 때 배운 내용)
 
 	for (i = 0; inputText[i] != 0; i++) {
-		printf("%d ", inputText[i]);
-		textCnt++;
+		/*printf("%d ", inputText[i]);*/
+		/**(pText + i) = inputText[i];*/
+		if (inputText[i] != 32) { // ASCII Code값이 32 (공백)이 아니라면,
+			textCnt++;
+		}else{
+			textSpaceCnt++;
+		}
 	}
 
-	printf("\n\n출력 : ");
-	printf("%d\n\n", textCnt);
-	for (i = textCnt; i > 0; i--) {
-		printf("%c", inputText[i-1]);
+	// Debugging: pText 출력하기.
+	/*printf("\n");
+	printf("%d, %d\n", textCnt, textSpaceCnt);*/
+	for (i = 0; i < textCnt + textSpaceCnt; i++) {
+		*(pText + i) = inputText[i];
+		/*printf("%d ", *(pText + i));*/
 	}
 
-	printf("\n%s", &inputText);
+	// 최다등장문자 구하는 알고리즘.
+	for (i = 0; i < textCnt + textSpaceCnt; i++) {
+		tempChar = *(pText + i);
+		if (*(pText + i) == 32) { //
+			continue;
+		}
+		for (j = 0; j < textCnt + textSpaceCnt; j++) {
+			if (tempChar == *(pText + j)) {
+				tempCmpCnt++;
+			}
+		}
+
+		if (tempCmpCnt > cmpCnt) {
+			cmpCnt = tempCmpCnt;
+			mostChar = tempChar;
+		}
+		tempCmpCnt = 0;
+	}
+
+	// 형식 출력하기.
+	printf("\n출력 : ");
+	for (i = textCnt + textSpaceCnt; i > 0; i--) {
+		printf("%c", *(pText + (i - 1)));
+	}
+	printf("\n       최다등장문자 : %c", mostChar);
 
 	return 0;
 }
