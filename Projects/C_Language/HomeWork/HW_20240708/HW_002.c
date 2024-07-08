3b930f30c19ba16008bd43fdcde89733b5d8cbf6
Kinesis <lordkinesis@naver.com>
2024-07-08 13:47:37 +0900

    Update: HW_002.c
    임시 저장.
    입력 받은 숫자로 배열 전체를 구성하는 시스템은 구현 완료. 이제 stack 기능을 활용하여 배열을 구성하도록 시스템을 설계하면 됨.

--- Projects/C_Language/HomeWork/HW_20240708/HW_002.c
+++ Projects/C_Language/HomeWork/HW_20240708/HW_002.c
@@ -4,8 +4,68 @@
 
 int main() {
 
+	// 변수 선언하기.
+	int numAry[4][3] = { 0, };
+	int* numList = (int*)calloc(12, sizeof(4)); // 입력되는 숫자 저장용(stack)
+	int i, j;
+	int inputNum, nextInput, idxCnt = 0;
+
+	// Debugging: 변경 전.
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d ", numAry[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 
 
+	// 형식 입출력 받기.
+	printf("input : ");
+	scanf("%d", &inputNum);
+	*(numList + idxCnt) = inputNum;
+
+	// (최초) 입력 받은 숫자로 배열 전체 지정하기.
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			numAry[i][j] = inputNum;
+		}
+	}
+
+	// Debugging: 변경 이후.
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d ", numAry[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+
+	while (1){
+
+		printf("next input : ");
+		scanf("%d", &nextInput);
+		idxCnt++;
+		*(numList + idxCnt) = nextInput;
+
+		// 입력 받은 숫자로 배열 전체 지정하기.
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 3; j++) {
+				numAry[i][j] = nextInput;
+			}
+		}
+
+		// Debugging: 변경 이후.
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 3; j++) {
+				printf("%d ", numAry[i][j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+	}
 
 	return 0;
 }
