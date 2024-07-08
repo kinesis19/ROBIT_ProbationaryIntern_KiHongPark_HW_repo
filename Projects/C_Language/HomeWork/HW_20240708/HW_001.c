c5267ad67f1e5139185260a531adf1e11d4ebb45
Kinesis <lordkinesis@naver.com>
2024-07-08 13:23:14 +0900

    Update: HW_001.c
    임시 저장.
    char type의 포인터를 두 개 사용하지 말고, 처음부터 하나의 char type의 포인터를 사용하도록 하는 알고리즘이 간편한 듯.

--- Projects/C_Language/HomeWork/HW_20240708/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240708/HW_001.c
@@ -1,48 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
 	// 변수 선언하기.
-	char inputText[] = { '\0', };
-	char* pText;
+	char* inputText = (char*)malloc(sizeof(char) * 20);
 	char tempChar, mostChar; // 최다 등장 비교용도.
 	int cmpCnt = 0, tempCmpCnt = 0;
 	int i, j, textCnt = 0, textSpaceCnt = 0;
 
-	*pText = inputText;
-
 	// 입출력 받기.
 	printf("입력 : ");
-	scanf("%[^\n]s", &inputText); // 공백으로 입력 받기 위한 연산자 (광운대 SW예비학교 때 배운 내용)
+	scanf("%[^\n]s", inputText); // 공백으로 입력 받기 위한 연산자 (광운대 SW예비학교 때 배운 내용)
 
-	for (i = 0; inputText[i] != 0; i++) {
-		/*printf("%d ", inputText[i]);*/
-		/**(pText + i) = inputText[i];*/
-		if (inputText[i] != 32) { // ASCII Code값이 32 (공백)이 아니라면,
+	printf("%s", inputText);
+	for (i = 0; i < 20; i++) {
+		printf("%d : %c\n", i, *(inputText + i));
+	}
+
+	for (i = 0; *(inputText + i); i++) {
+		if (*(inputText + i) != 32) { // ASCII Code값이 32 (공백)이 아니라면,
 			textCnt++;
-		}else{
+		}else {
 			textSpaceCnt++;
 		}
 	}
 
-	// Debugging: pText 출력하기.
-	printf("\n");
-	/*printf("%c %c", *(pText + 0), *(pText + 1));*/
-	/*printf("%d, %d\n", textCnt, textSpaceCnt);*/
-	for (i = 0; i < textCnt + textSpaceCnt; i++) {
-		*(pText + i) = inputText[i];
-		/*printf("%d ", *(pText + i));*/
-	}
 
-	// 최다등장문자 구하는 알고리즘.
+	//// 최다등장문자 구하는 알고리즘.
 	for (i = 0; i < textCnt + textSpaceCnt; i++) {
-		tempChar = *(pText + i);
-		if (*(pText + i) == 32) { // 공백은 최다등장문자에 포함하지 않음.
+		tempChar = *(inputText + i);
+		if (*(inputText + i) == 32) { // 공백은 최다등장문자에 포함하지 않음.
 			continue;
 		}
 		for (j = 0; j < textCnt + textSpaceCnt; j++) {
-			if (tempChar == *(pText + j)) {
+			if (tempChar == *(inputText + j)) {
 				tempCmpCnt++;
 			}
 		}
@@ -56,10 +49,15 @@ int main() {
 
 	// 형식 출력하기.
 	printf("\n출력 : ");
+	// 입력 받은 문자열 거꾸로 출력하기.
 	for (i = textCnt + textSpaceCnt; i > 0; i--) {
-		printf("%c", *(pText + (i - 1)));
+		printf("%c", *(inputText + (i - 1)));
 	}
+	// 최다등장문자 출력하기. (공백 제외)
 	printf("\n       최다등장문자 : %c", mostChar);
+
+	// 동적할당 메모리 해제하기.
+	free(inputText);
 
 	return 0;
 }
