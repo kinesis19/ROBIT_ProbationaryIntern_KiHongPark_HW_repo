c4a443b47f4279f70206f9f77ef48c865c395b25
Kinesis <lordkinesis@naver.com>
2024-07-08 14:56:40 +0900

    Update: HW_003.c
    임시 저장.
    명령어별로 조건문을 나눠 놓음.
    사용자 지정 함수를 사용하여 각 명령어에 따른 처리를 하면 될 듯.

--- Projects/C_Language/HomeWork/HW_20240708/HW_003.c
+++ Projects/C_Language/HomeWork/HW_20240708/HW_003.c
@@ -1,8 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
+typedef struct _Node {
+	int data;
+	struct _Node* next;
+}Node;
+
+typedef struct _LinkedList {
+	Node* head;
+	Node* tail;
+	Node* Cur;
+	int size;
+}LinkedList;
+
+
+
 int main() {
+	// 단순 연결 리스트 구조체 선언하기.
+	LinkedList linkedList;
+
+	// 변수 선언하기.
+	char* inputCmd = (char*)malloc(sizeof(char) * 20);
+	
+	// 명령어(cmd)을 입력 받고, 각 명령에 따라 정해진 작업을 진행함.
+	while (1){
+		scanf("%s", inputCmd);
+
+		if (strcmp(inputCmd, "insert") == 0) {
+			printf("insert!");
+		}else if (strcmp(inputCmd, "insert_back") == 0) {
+			printf("insert_back!");
+		}else if (strcmp(inputCmd, "insert_first") == 0) {
+			printf("insert_first!");
+		}else if (strcmp(inputCmd, "delete") == 0) {
+			printf("delete!");
+		}else if (strcmp(inputCmd, "delete_first") == 0) {
+			printf("delete_first!");
+		}else if (strcmp(inputCmd, "delete_back") == 0) {
+			printf("delete_back!");
+		}else if (strcmp(inputCmd, "get_entry") == 0) {
+			printf("get_entry!");
+		}else if (strcmp(inputCmd, "get_length") == 0) {
+			printf("get_length!");
+		}else if (strcmp(inputCmd, "print_list") == 0) {
+			printf("print_list!");
+		}else if (strcmp(inputCmd, "reverse") == 0) {
+			printf("reverse!");
+		}else if (strcmp(inputCmd, "Data") == 0) {
+			printf("Data!");
+		}
+
+		printf("\n");
+	}
+
 
 	return 0;
 }
