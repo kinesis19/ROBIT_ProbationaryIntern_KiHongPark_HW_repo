1deecce699c993391a7917e67a4cdebec9d10e8c
Kinesis <lordkinesis@naver.com>
2024-07-08 13:26:28 +0900

    Update: Others

--- Projects/C_Language/Practicing/FistProject/main.c
+++ Projects/C_Language/Practicing/FistProject/main.c
@@ -1,33 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define SIZE 2
-
-typedef struct _Student {
-
-	int number;
-	char name[10];
-	double grade;
-}Student;
+#include <stdlib.h>
 
 int main() {
-	Student list[SIZE];
-	int i = 0;
 
-	printf("%d", sizeof(list[SIZE]));
+	char* text = (char*)malloc(sizeof(char) * 20);
 
+	printf("입력 : ");
+	scanf("%[^\n]s", text);
 
-	for (i = 0; i < SIZE; i++) {
-		printf("학번을 입력하시오: ");
-		scanf("%d", &list[i].number);
-		printf("이름을 입력하시오: ");
-		scanf("%s", &list[i].name);
-		printf("학점을 입력하시오: ");
-		scanf("%lf", &list[i].grade);
-	}
-
-	for (i = 0; i < SIZE; i++) {
-		printf("학번 : %d, 이름 : %s, 학점 : %lf\n", list[i].number, list[i].name, list[i].grade);
-	}
+	printf("%s", text);
 
 
 	return 0;
