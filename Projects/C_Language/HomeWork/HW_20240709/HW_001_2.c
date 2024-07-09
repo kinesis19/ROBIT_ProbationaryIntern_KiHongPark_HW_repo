400bc0023d897588f5be4bdedb6aeddd1bfa8fe6
Kinesis <lordkinesis@naver.com>
2024-07-09 21:51:31 +0900

    Update: Files
    파일 이름 변경.
    과제1-1 : 포인터, 배열, 함수 등 개념 공부한 것 정리하기.
    과제1-2 : p.15 소스코드 분석하여 실행 로직 설명하기.
    과제1-3 : p.25 소스코드 분석하여 실행 로직 설명하기.

--- Projects/C_Language/HomeWork/HW_20240709/HW_001_2.c
+++ Projects/C_Language/HomeWork/HW_20240709/HW_001_2.c
@@ -1,21 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void SetStr(char** str);
+void myPrint1(int* a, int row, int col);
+void myPrint2(int a[4][3], int row, int col);
+void myPrint3(int a[][3], int row, int col);
+void myPrint4(int* (a)[3], int row, int col);
 
 int main() {
 
-	char* str;
+	int arr[4][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12} };
+	int row = sizeof(arr) / sizeof(arr[0]);
+	int col = sizeof(arr[0]) / sizeof(arr[0][0]);
 
-	str = "I'am sad";
-	
-	SetStr(&str);
-
-	printf("%s", str);
+	myPrint1(arr, row, col);
+	myPrint2(arr, row, col);
+	myPrint3(arr, row, col);
+	myPrint4(arr, row, col);
 
 	return 0;
 }
 
-void SetStr(char** str) {
-	*str = "I'am happy";
+
+void myPrint1(int* a, int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) printf("%d ", *(a + i * col + j));
+	}
+	printf("\n");
+}
+void myPrint2(int a[4][3], int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) printf("%d ", a[i][j]);
+	}
+	printf("\n");
+}
+void myPrint3(int a[][3], int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) printf("%d ", a[i][j]);
+	}
+	printf("\n");
+}
+void myPrint4(int* (a)[3], int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) printf("%d ", *(*(a + i) + j));
+	}
+	printf("\n");
 }
