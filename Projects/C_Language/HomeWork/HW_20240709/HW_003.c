df077060910d5cd046a8ac9e0c4576c5c718f332
Kinesis <lordkinesis@naver.com>
2024-07-10 10:17:37 +0900

    Create: HW_003.c
    임시 저장.
    2차원 배열 동적할당 기능 구현 중.

---
+++ Projects/C_Language/HomeWork/HW_20240709/HW_003.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void print(int* row, int* col, int** pArr);
+void arr_ij(int* sizeRow, int* sizeCol, int** pArr);
+
+
+int main() {
+
+	// 더블포인터 변수 및 일반 변수 선언하기.
+	int** arr = NULL;
+	int row, col, sizeRow, sizeCol;
+	
+	// 입출력 받기.
+	printf("열의 수를 입력하세요:");
+	scanf("%d", &sizeCol);
+	printf("행의 수를 입력하세요:");
+	scanf("%d", &sizeRow);
+
+	// 변수 할당하기.
+	row = sizeRow;
+	col = sizeCol;
+
+	// 2차원 동적 메모리 할당하기.
+	arr_ij(&sizeRow, &sizeCol, arr);
+
+	// 배열 arr 출력하기.
+	print(&row, &col, arr);
+
+	// 동적 메모리 해제하기.
+	for (int i = 0; i < row; i++) {
+		free(arr[i]);
+	}
+
+	return 0;
+}
+// 달팽이 형식에 맞게 출력하는 함수.
+void print(int* row, int* col, int** pArr) {
+
+}
+
+// 당팽이 형식에 맞게 구현하는 함수.
+void arr_ij(int* sizeRow, int* sizeCol, int** pArr) {
+
+	printf("%d %d\n\n", *sizeRow, *sizeCol);
+	printf("%d\n\n", sizeof(int) * (*sizeRow));
+	// 2차원 동적 메모리 할당하기.
+	pArr = (int**)malloc(sizeof(int) * (*sizeRow));
+	for (int i = 0; i < *sizeCol; i++) {
+		printf("%d ", i);
+	}
+
+}
+
+
