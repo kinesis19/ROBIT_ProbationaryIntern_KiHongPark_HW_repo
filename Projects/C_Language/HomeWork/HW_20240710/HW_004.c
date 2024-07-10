5db4013267433366d71eed87946b127c8152d266
Kinesis <lordkinesis@naver.com>
2024-07-11 03:45:43 +0900

    Update: HW_004.c
    임시 저장.
    이제 회문 판별 알고리즘을 구현해 내면 됨.

--- Projects/C_Language/HomeWork/HW_20240710/HW_004.c
+++ Projects/C_Language/HomeWork/HW_20240710/HW_004.c
@@ -4,21 +4,21 @@
 
 // Stack 구조체 정의하기.
 typedef struct _textStack {
-	int max; // stack의 용량 저장.
+	int max; // stack의 용량 저장. (동적)
 	char ptr; // stack의 포인터.
 	char qPtr; // stack에서 Deque를 하기 위한 포인터. Queue 전용 Struct를 만드는 것 말고, 동일한 Struct에서 Deque가 되도록 시도해 봄.
 	char* stk[]; // stack을 가리키는 포인터.
 }TextStack;
 
 // 필요한 사용자 정의 함수를 프로토타입으로 선언하기.
-void InitializingStack(TextStack* tStack, char* text); // Stack 초기화 함수.
+void InitializingStack(TextStack* tStack, char* text, int* textCnt); // Stack 초기화 함수.
 void PushingStack(TextStack* tStack, char* text); // Stack에 push하는 함수.
-void PoppingStack(TextStack* tStack, char* text); // Stack에 pop하는 함수.
+char PoppingStack(TextStack* tStack, char* text); // Stack에 pop하는 함수.
 /* Stack에서 Deque하는 함수.
 * 원래는 Queue에서 Deque를 해야 하지만,
 * 미리 선언한 Stack에 Deque 기능을 추가하는 것으로 설계함.
 */ 
-void DequeueingStack(TextStack* tStack, char* text); 
+char DequeueingStack(TextStack* tStack, char* text); 
 
 int main() {
 
@@ -28,6 +28,10 @@ int main() {
 	inputText = (char*)malloc(sizeof(char) * 100);
 
 	TextStack* textStack = (TextStack*)malloc(sizeof(TextStack));
+	
+	// inputText의 총 갯수를 저장하기 위한 변수.
+	int temp = 0;
+	int* textCnt = &temp;
 
 	// 형식 입출력하기.
 	printf("회문 여부를 파악하고 싶은 문자열 입력 : ");
@@ -35,13 +39,22 @@ int main() {
 
 	printf("\n\n");
 	// Stack 초기화하기.
-	InitializingStack(textStack, inputText);
+	InitializingStack(textStack, inputText, textCnt);
+
+	printf("%d", *textCnt);
 	// inputText 전체를 Stack에 Push하기.
 	PushingStack(textStack, inputText);
 	// Stack에서 Pop하기 = 뒤에서 하나 빼기.
 	PoppingStack(textStack, inputText);
 	// Stack에서 Deque하기 = 앞에서 하나 빼기.
 	DequeueingStack(textStack, inputText);
+
+	
+
+	for (int i = 0; i < *textCnt; i++) {
+
+	}
+
 
 	textStack->ptr = 0;
 	for (int i = 0; *(inputText + i) != NULL; i++) {
@@ -55,7 +68,7 @@ int main() {
 
 	return 0;
 }
-void InitializingStack(TextStack* tStack, char* text) {
+void InitializingStack(TextStack* tStack, char* text, int* textCnt) {
 
 	int cnt = 0;
 	for (int i = 0; *(text + i) != NULL; i++) {
@@ -64,6 +77,7 @@ void InitializingStack(TextStack* tStack, char* text) {
 	tStack->ptr = 0; // stack 초기화 시, stack의 pointer는 0으로 초기화 함.
 	tStack->qPtr = 0; // stack 초기화 시, queue의 pointer는 0으로 초기화 함.
 	tStack->max = cnt; // stack의 용량은 입력된 문자열의 문자 수로 지정함.
+	*textCnt = cnt;
 }
 
 void PushingStack(TextStack* tStack, char* text) {
@@ -74,7 +88,7 @@ void PushingStack(TextStack* tStack, char* text) {
 		}
 	}
 }
-void PoppingStack(TextStack* tStack, char* text) { // 하나씩 pop 하기.
+char PoppingStack(TextStack* tStack, char* text) { // 하나씩 pop 하기.
 	tStack->ptr = tStack->max - 1;
 	if (0 < tStack->ptr) { // stack의 용량 범위 이내라면 push를 진행함.
 		tStack->stk[tStack->ptr] = NULL;
@@ -82,7 +96,7 @@ void PoppingStack(TextStack* tStack, char* text) { // 하나씩 pop 하기.
 	}
 }
 
-void DequeueingStack(TextStack* tStack, char* text) { // 하나씩 deque 하기.
+char DequeueingStack(TextStack* tStack, char* text) { // 하나씩 deque 하기.
 	if (tStack->qPtr < tStack->max) { // stack의 용량 범위 이내라면 push를 진행함.
 		tStack->stk[tStack->qPtr] = NULL;
 		tStack->qPtr++;
