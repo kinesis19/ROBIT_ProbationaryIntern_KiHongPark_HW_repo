72cbb20104ab264ad5b082557fe734a7bf94ef9b
Kinesis <lordkinesis@naver.com>
2024-07-10 21:31:27 +0900

    Update: Practicing

--- Projects/C_Language/Practicing/PracticingProject/Main.c
+++ Projects/C_Language/Practicing/PracticingProject/Main.c
@@ -1,47 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void myPrint1(int* a, int row, int col);
-void myPrint2(int a[4][3], int row, int col);
-void myPrint3(int a[][3], int row, int col);
-void myPrint4(int *(a)[3], int row, int col);
+typedef struct _Date {
+	int month;
+	int day;
+	int year;
+}Date;
+
+typedef struct _Student {
+	int number;
+	char name[10];
+	double grade;
+	Date* mdy;
+}Student;
 
 int main() {
 
-	int arr[4][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12} };
-	int row = sizeof(arr) / sizeof(arr[0]);
-	int col = sizeof(arr[0]) / sizeof(arr[0][0]);
+	Student s = { 44, "Kim", 4.5 };
+	Date d = { 6, 26, 1995 };
 
-	myPrint1(arr, row, col);
-	myPrint2(arr, row, col);
-	myPrint3(arr, row, col);
-	myPrint4(arr, row, col);
+	s.mdy = &d;
+
+	printf("학번 : %d 이름 : %s 학점 : %.1f\n", s.number, s.name, s.grade);
+	printf("생년월일 : %d년 %d월 %d일\n", s.mdy->year, s.mdy->month, s.mdy->day);
 
 	return 0;
-}
-
-
-void myPrint1(int* a, int row, int col) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) printf("%d ", *(a + i * col + j));
-	}
-	printf("\n");
-}
-void myPrint2(int a[4][3], int row, int col) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) printf("%d ", a[i][j]);
-	}
-	printf("\n");
-}
-void myPrint3(int a[][3], int row, int col) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) printf("%d ", a[i][j]);
-	}
-	printf("\n");
-}
-void myPrint4(int* (a)[3], int row, int col) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) printf("%d ", *(*(a+i) + j));
-	}
-	printf("\n");
 }
