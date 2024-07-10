04311dcc40af44504e34734616950ba8bbd5d628
Kinesis <lordkinesis@naver.com>
2024-07-10 16:21:13 +0900

    Update: HW_003.c
    임시 저장.

--- Projects/C_Language/HomeWork/HW_20240709/HW_003.c
+++ Projects/C_Language/HomeWork/HW_20240709/HW_003.c
@@ -24,8 +24,10 @@ int main() {
 	// 2차원 동적 메모리 할당하기.
 	arr_ij(&sizeRow, &sizeCol, arr);
 
+	if (arr == NULL) printf("동적 할당 되지 않았습니다.");
+	else if (arr != NULL) printf("정상적으로 동적 할당 되었습니다.");
 	// 배열 arr 출력하기.
-	/*print(&row, &col, arr);*/
+	/*print(&row, &col, &arr);*/
 
 	// 동적 메모리 해제하기.
 	for (int i = 0; i < row; i++) {
@@ -37,6 +39,11 @@ int main() {
 
 // 달팽이 형식에 맞게 출력하는 함수.
 void print(int* row, int* col, int** pArr) {
+
+	pArr = (int**)malloc(sizeof(int) * (*row));
+
+	/*printf("%d %d %d", *row, *col, sizeof(*(*(pArr + 0) + 1)));*/
+	printf("%d %d %d", *row, *col, sizeof(*(pArr + 0)));
 
 	for (int i = 0; i < *row; i++) {
 		for (int j = 0; j < *col; j++) {
@@ -50,29 +57,41 @@ void print(int* row, int* col, int** pArr) {
 // 당팽이 형식에 맞게 구현하는 함수.
 void arr_ij(int* sizeRow, int* sizeCol, int** pArr) {
 
-	int cnt = 1;
+	int inputNum = 1; // 배열에 넣을 숫자 변수.
+	int change = 0; // change가 10이 될 때마다 행과 열의 번호를 삽입함.
 	// 2차원 배열 만들기.
+	// 1, sizeRow 만큼의 포인터 배열을 동적 할당함.
 	pArr = (int**)malloc(sizeof(int) * (*sizeRow));
 	if (pArr == NULL) printf("동적할당 실패함.\n");
 	for (int i = 0; i < (*sizeRow); i++) {
-		pArr[i] = (int*)malloc(sizeof(int) * (*sizeCol));
+		// 2. 각각의 포인터 배열 요소에 sizeCol 만큼의 포인터 배열을 동적 할당함.
+		*(pArr + i) = (int*)malloc(sizeof(int) * (*sizeCol));
 	}
 
+	int cntRow = 0, cntCol = 0;
+
 	for (int i = 0; i < (*sizeRow); i++) {
+		change = 0;
 		for (int j = 0; j < (*sizeCol); j++) {
-			*(*(pArr + i) + j) = cnt;
-			cnt++;
+			int tempInputNum = inputNum;
+			if (change == (*sizeRow) - 1) {
+				inputNum = 999;
+			}
+			*(*(pArr + i) + j) = inputNum;
+			inputNum = tempInputNum;
+			change++;
+			inputNum++;
 		}
 	}
 
 	for (int i = 0; i < (*sizeRow); i++) {
 		for (int j = 0; j < (*sizeCol); j++) {
-			printf("%d ", *(*(pArr + i) + j));
+			printf("%3d[%d][%d] ", *(*(pArr + i) + j), i, j);
 		}
 		printf("\n");
 	}
-
-	print(sizeRow, sizeCol, pArr);
+	printf("\n\n%d\n\n", *(*(pArr + 0) + 9));
+	/*print(sizeRow, sizeCol, pArr);*/
 
 }
 
