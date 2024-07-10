2e5749bd98b2d7c97bc88743ce5ce97f05111eaf
Kinesis <lordkinesis@naver.com>
2024-07-10 15:25:10 +0900

    Update: HW_003.c
    print() 함수를 arr_ij() 함수 내부에서 호출하면 원하는대로 출력이 가능함.
    
    근데 이건 call by value 형식임.
    과제에서는 call by reference 형식으로 주소를 참조 받아서 출력을 원하는 것 같은데, 과제에 제시된 코드가 정확한지 궁금함.

--- Projects/C_Language/HomeWork/HW_20240709/HW_003.c
+++ Projects/C_Language/HomeWork/HW_20240709/HW_003.c
@@ -23,43 +23,28 @@ int main() {
 
 	// 2차원 동적 메모리 할당하기.
 	arr_ij(&sizeRow, &sizeCol, arr);
-	int num = 10;
-	int* temp;
-
-	temp = &num;
-	arr = &temp;
-
-	if (arr == NULL) printf("arr의 동적할당 실패함.\n");
-	printf("aaaaaaaa\n");
-	/*for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			printf("hhhhhh");
-			printf("%3d\n ", arr[i][j]);
-		}
-		printf("\n");
-	}*/
 
 	// 배열 arr 출력하기.
 	/*print(&row, &col, arr);*/
 
 	// 동적 메모리 해제하기.
-	/*for (int i = 0; i < row; i++) {
+	for (int i = 0; i < row; i++) {
 		free(arr[i]);
-	}*/
+	}
 
 	return 0;
 }
 
 // 달팽이 형식에 맞게 출력하는 함수.
 void print(int* row, int* col, int** pArr) {
-	printf("bbb");
-	printf("%3d ", pArr[0][0]);
+
 	for (int i = 0; i < *row; i++) {
 		for (int j = 0; j < *col; j++) {
-			printf("%3d ", pArr[i][j]);
+			printf("%3d ", *(*(pArr + i) + j));
 		}
 		printf("\n");
 	}
+
 }
 
 // 당팽이 형식에 맞게 구현하는 함수.
@@ -67,52 +52,27 @@ void arr_ij(int* sizeRow, int* sizeCol, int** pArr) {
 
 	int cnt = 1;
 	// 2차원 배열 만들기.
-	int** ary = (int**)malloc(sizeof(int) * (*sizeRow) * (*sizeCol));
+	pArr = (int**)malloc(sizeof(int) * (*sizeRow));
+	if (pArr == NULL) printf("동적할당 실패함.\n");
 	for (int i = 0; i < (*sizeRow); i++) {
-		ary[i] = (int*)malloc(sizeof(int) * (*sizeCol));
+		pArr[i] = (int*)malloc(sizeof(int) * (*sizeCol));
 	}
-	
+
 	for (int i = 0; i < (*sizeRow); i++) {
 		for (int j = 0; j < (*sizeCol); j++) {
-			ary[i][j] = cnt;
+			*(*(pArr + i) + j) = cnt;
 			cnt++;
 		}
 	}
 
 	for (int i = 0; i < (*sizeRow); i++) {
 		for (int j = 0; j < (*sizeCol); j++) {
-			printf("%d ", ary[i][j]);
+			printf("%d ", *(*(pArr + i) + j));
 		}
 		printf("\n");
 	}
 
-
-	// 1. 행 만큼 동적 할당하기.
-
-	//if (arr == NULL) printf("동적할당 실패함.\n");
-	
-
-	// 2차원 동적 메모리 할당하기.
-	// 1. 행의 갯수만큼 2차원 배열 동적 할당하기.
-	//pArr = (int**)malloc(sizeof(int) * (*sizeRow)); // 10개의 포인터 변수 선언함. = pArr은 10개의 포인터 변수를 가지고 있음.
-	//pArr = (int**)calloc(*sizeRow, sizeof(int));
-	//if (pArr == NULL) printf("동적할당 실패함.\n");
-	//// 2. 각 행별 열의 갯수만큼 동적 할당하기.
-	//for (int i = 0; i < *sizeRow; i++) {
-	//	//*(pArr + i) = (int*)malloc(sizeof(int) * (*sizeCol)); // 선언된 10개의 포인터 변수에 각각 10개의 히위 포인터 변수를 선언함.
-	//	*(pArr + i) = (int*)calloc(*sizeCol, sizeof(int));
-	//}
-
-	//if (*(*(pArr) + 0) == NULL) printf("하위 동적할당 실패함.\n");
-	//int cnt = 1;
-	//for (int i = 0; i < *sizeRow; i++) {
-	//	for (int j = 0; j < *sizeCol; j++) {
-	//		*(*(pArr + i) + j) = cnt;
-	//		cnt++;
-	//		printf("%3d ", *(*(pArr + i) + j));
-	//	}
-	//	printf("\n");
-	//}
+	print(sizeRow, sizeCol, pArr);
 
 }
 
