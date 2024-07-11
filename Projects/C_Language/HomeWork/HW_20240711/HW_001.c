ccefa61c52ba7921fbf14e8e79a7751e1dee4140
Kinesis <lordkinesis@naver.com>
2024-07-12 01:53:43 +0900

    Update: HW_001.c
    임시 저장.
    
    TODO:
    1. 예외 처리 기능 구현.
    2. 예외 처리 통과 시 -> Student에 값 할당하기.

--- Projects/C_Language/HomeWork/HW_20240711/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240711/HW_001.c
@@ -5,11 +5,11 @@
 // 학생 구조체
 typedef struct _Student {
 	int number; // 번호
-	char name[20]; // 이름
-	char adrCountry[20]; // 국가
-	char adrState[20]; // 도
-	char adrCity[20]; // 시
-	char adrTown[20]; // 구
+	char name[100]; // 이름
+	char adrCountry[100]; // 국가
+	char adrDo[100]; // 도
+	char adrSi[100]; // 시
+	char adrGu[100]; // 구
 	double grade; // 성적
 	struct _Student* next; // 다음 학생의 주소
 }Student;
@@ -22,8 +22,9 @@ typedef struct _StudentList {
 	int size;
 }StudentList;
 
-void InitializingStudentList(StudentList* stdList); // StudentList 초기화 함수.
-void AddingStudent(StudentList* stdList);
+void Initializing_StudentList(StudentList* stdList); // StudentList 초기화 함수.
+void Adding_Student(StudentList* stdList);
+void ExceptionHandling_Adding(char type);
 
 
 int main() {
@@ -33,11 +34,16 @@ int main() {
 	char* inputCommand = (char*)malloc(sizeof(char) * 20);
 
 
-	InitializingStudentList(studentList);
-	printf("[SYSTEM]출석부가 초기화 되었습니다.\n\n");
+	Initializing_StudentList(studentList);
+	if (studentList == NULL) {
+		printf("[SYSTEM]출석부가 초기화 되지 않았습니다.");
+	}else {
+		printf("[SYSTEM]출석부가 초기화 되었습니다.");
+
+	}
 
 	while (1){
-		printf("|---------------------------------------------|\n");
+		printf("\n\n|---------------------------------------------|\n");
 		printf("|     [로빛 19기 수습단원 출석부 시스템]      |\n");
 		printf("|---------------------------------------------|\n");
 		printf("|           사용 가능한 명령어 모음           |\n");
@@ -49,17 +55,7 @@ int main() {
 		scanf("%s", inputCommand);
 
 		if(strcmp(inputCommand, "add") == 0){
-			int num;
-			char name[20] = { '\0', }, adrCountry[20] = { '\0', }, adrState[20] = { '\0', }, adrCity[20] = { '\0', }, adrTown[20] = { '\0', };
-			double grade;
-
-			printf("[SYSTEM]추가할 학생의 번호를 입력하세요 : ");
-			scanf("%d", &num);
-			printf("[SYSTEM]추가할 학생의 이름을 입력하세요 : ");
-			scanf("%s", &name);
-			printf("[SYSTEM]추가할 학생의 이름을 입력하세요 : ");
-			scanf("%s", &name);
-
+			Adding_Student(studentList);
 		}else if (strcmp(inputCommand, "delete") == 0) {
 
 		}else if (strcmp(inputCommand, "search") == 0) {
@@ -81,13 +77,38 @@ int main() {
 	return 0;
 }
 
-void InitializingStudentList(StudentList* stdList){
+void Initializing_StudentList(StudentList* stdList){
 	stdList->size = 0;
 	stdList->top = -1; // Stack의 top의 초기 상태는 -1 상태임.
 	stdList->cur = NULL;
 }
 
-void AddingStudent(StudentList* stdList) {
+void Adding_Student(StudentList* stdList) {
+	int num;
+	char name[20] = { '\0', }, adrCountry[100] = { '\0', }, adrDo[100] = { '\0', }, adrSi[100] = { '\0', }, adrGu[100] = { '\0', };
+	double grade;
 
+	printf("[SYSTEM]추가할 학생의 번호를 입력하세요 : ");
+	scanf("%d", &num);
+	// 예외처리 함수 호출하기. -> num이 숫자인지 아닌지.
+	printf("[SYSTEM]추가할 학생의 이름을 입력하세요 : ");
+	scanf(" %[^\n]s", &name);
+	// 예외처리 함수 호출하기. -> name이 문자(열)인지 아닌지.
+	printf("[SYSTEM]추가할 학생의 거주하고 있는 국가를 입력하세요 : ");
+	scanf(" %[^\n]s", &adrCountry);
+	// 예외처리 함수 호출하기. -> addCountry가 문자(열)인지 아닌지.
+	printf("[SYSTEM]추가할 학생의 거주하고 있는 도를 입력하세요 : ");
+	scanf(" %[^\n]s", &adrDo);
+	// 예외처리 함수 호출하기. -> adrDo가 문자(열)인지 아닌지.
+	printf("[SYSTEM]추가할 학생의 거주하고 있는 시를 입력하세요 : ");
+	scanf(" %[^\n]s", &adrSi);
+	// 예외처리 함수 호출하기. -> adrSi가 문자(열)인지 아닌지.
+	printf("[SYSTEM]추가할 학생의 거주하고 있는 구를 입력하세요 : ");
+	scanf(" %[^\n]s", &adrGu);
+	// 예외처리 함수 호출하기. -> adrGu가 문자(열)인지 아닌지.
+	printf("[SYSTEM]추가할 학생의 성적을 입력하세요 : ");
+	scanf(" %lf", &grade);
+	// 예외처리 함수 호출하기. -> grade가 숫자인지 아닌지.
+	printf("%d, %s, %s, %s, %s, %s, %.1lf\n\n", num, name, adrCountry, adrDo, adrSi, adrGu, grade);
 }
 
