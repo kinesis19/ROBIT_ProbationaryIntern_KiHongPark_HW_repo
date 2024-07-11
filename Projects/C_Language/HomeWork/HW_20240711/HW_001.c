fbd49fc68b33829a6c58c55d702a97a5a6f59b62
Kinesis <lordkinesis@naver.com>
2024-07-12 05:45:54 +0900

    Update: HW_001.c
    add 기능은 다 구현한 듯.
    + print 기능도 구현함 -> 디버깅용.

--- Projects/C_Language/HomeWork/HW_20240711/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240711/HW_001.c
@@ -10,20 +10,24 @@ typedef struct _Student {
 	char* adrDo; // 도
 	char* adrSi; // 시
 	char* adrGu; // 구
-	double grade; // 성적
+	int grade; // 성적
 	struct _Student* next; // 다음 학생의 주소
 }Student;
 
 // 학생 리스트
 typedef struct _StudentList {
-	Student* top;
+	Student* head;
 	Student* cur;
+	Student* tail;
 	int* data[100];
 	int size;
 }StudentList;
 
 void Initializing_StudentList(StudentList* stdList); // StudentList 초기화 함수.
 void Adding_Student(StudentList* stdList); // 학생 추가 함수.
+void Printing_StudentList(StudentList* stdList);
+int Getting_IsitNumber(char* text, int cnt);
+int Changing_StringToInt(char* text);
 
 
 int main() {
@@ -48,14 +52,13 @@ int main() {
 		printf("|           사용 가능한 명령어 모음           |\n");
 		printf("|---------------------------------------------|\n");
 		printf("|  1. add         2.delete          3.search  |\n");
-		printf("|  4. sort        5.SaveAndLoad     6.Exit    |\n");
+		printf("|  4. sort        5.SaveAndLoad     6.exit    |\n");
 		printf("|---------------------------------------------|\n\n\n");
 		printf("[SYSTEM]명령어를 입력하세요 : ");
 		scanf("%s", inputCommand);
 
 		if(strcmp(inputCommand, "add") == 0){
 			Adding_Student(studentList);
-			printf("%d", studentList->top->number);
 		}else if (strcmp(inputCommand, "delete") == 0) {
 
 		}else if (strcmp(inputCommand, "search") == 0) {
@@ -65,8 +68,10 @@ int main() {
 		}else if (strcmp(inputCommand, "SaveAndLoad") == 0) {
 
 
-		}else if (strcmp(inputCommand, "Exit") == 0) {
+		}else if (strcmp(inputCommand, "exit") == 0) {
 
+		}else if (strcmp(inputCommand, "print") == 0) { // Debugging:
+			Printing_StudentList(studentList);
 		}
 
 	}
@@ -79,62 +84,148 @@ int main() {
 
 void Initializing_StudentList(StudentList* stdList){
 	stdList->size = 0;
-	stdList->top = -1; // Stack의 top의 초기 상태는 -1 상태임.
+	stdList->head = NULL;
 	stdList->cur = NULL;
+	stdList->tail = NULL;
+}
+
+int Getting_IsitNumber(char* text, int cnt) {
+	for (int i = 0; i < cnt; i++) {
+		if (text[i] < '0' || text[i] > '9') {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int Changing_StringToInt(char* text) {
+	/* Student의 number와 grade를 할당하는 데 사용함.
+	* 등급은 정수 형태의 등급. 1등급부터 ~ n등급까지 존재함. (1등급이 제일 낮은 것으로 하겠음)
+	*/
+	int result = 0;
+	for (int i = 0; text[i] != '\0'; i++) {
+		result = result * 10 + (text[i] - '0'); 
+		/*
+		* ASCII CODE에서 0은 48임.
+		* 입력된 문자 Type의 숫자에서 숫자의 기본인 '0'을 빼주면 해당 문자의 숫자가 return 됨.
+		* '1' - '0' = 49 - 48 = 1.
+		*/ 
+	}
+	return result;
 }
 
 void Adding_Student(StudentList* stdList) {
-	int num;
+
 	char name[100] = { '\0', }, adrCountry[100] = { '\0', }, adrDo[100] = { '\0', }, adrSi[100] = { '\0', }, adrGu[100] = { '\0', };
-	double grade;
 
 	Student* student = (Student*)malloc(sizeof(Student));
 
-	stdList->size++;
+	char text[100];
+	int cntNum = 0, cntGrade = 0;
 	printf("[SYSTEM]추가할 학생의 번호를 입력하세요(1부터 ~ 100사이) : ");
-	scanf("%d", &num);
-	// 임시로 구현한 예외처리.
-	getchar();
-	while (num < 1 || num > 100) {
-		printf("다시 입력하세요 : ");
-		scanf("%d", &num);
-		getchar();
+	scanf("%s", text);
+
+	for (int i = 0; text[cntNum] != '\0'; i++) {
+		cntNum++;
 	}
-	student->number = num;
-	// 예외처리 함수 호출하기. -> num이 숫자인지 아닌지.
+	while (!Getting_IsitNumber(text, cntNum)) {
+		printf("숫자만 입력하세요 : ");
+		scanf("%s", text);
+		cntNum = 0;
+		for (int i = 0; text[cntNum] != '\0'; i++) {
+			cntNum++;
+		}
+	}
+	student->number = Changing_StringToInt(text);
 
 	printf("[SYSTEM]추가할 학생의 이름을 입력하세요 : ");
 	scanf(" %[^\n]s", &name);
-	student->name = name;
-	// 예외처리 함수 호출하기. -> name이 문자(열)인지 아닌지.
+	int cntName = 0;
+	for (int i = 0; name[cntName] != '\0'; i++) {
+		cntName++;
+	}
+	student->name = (char*)malloc(cntName + 1); // 이름에 동적할당하기.
+	for (int i = 0; i <= cntName; i++) { // 동적할당한 student의 name에 이곳 name의 각각의 문자를 할당함.
+		student->name[i] = name[i];
+	}
 
 	printf("[SYSTEM]추가할 학생의 거주하고 있는 국가를 입력하세요 : ");
 	scanf(" %[^\n]s", &adrCountry);
-	student->adrCountry = adrCountry;
-	// 예외처리 함수 호출하기. -> addCountry가 문자(열)인지 아닌지.
+	int cntCountry = 0;
+	for (int i = 0; adrCountry[cntCountry] != '\0'; i++) {
+		cntCountry++;
+	}
+	student->adrCountry = (char*)malloc(cntCountry + 1); // 국가에 동적할당하기.
+	for (int i = 0; i <= cntCountry; i++) { // 동적할당한 student의 adrCountry에 이곳 adrCountry의 각각의 문자를 할당함.
+		student->adrCountry[i] = adrCountry[i];
+	}
 
 	printf("[SYSTEM]추가할 학생의 거주하고 있는 도를 입력하세요 : ");
 	scanf(" %[^\n]s", &adrDo);
-	student->adrDo = adrDo;
-	// 예외처리 함수 호출하기. -> adrDo가 문자(열)인지 아닌지.
+	int cntDo = 0;
+	for (int i = 0; adrDo[cntDo] != '\0'; i++) {
+		cntDo++;
+	}
+	student->adrDo = (char*)malloc(cntDo + 1); // 주소-도에 동적할당하기.
+	for (int i = 0; i <= cntDo; i++) { // 동적할당한 student의 adrDo에 이곳 adrDo의 각각의 문자를 할당함.
+		student->adrDo[i] = adrDo[i];
+	}
 
 	printf("[SYSTEM]추가할 학생의 거주하고 있는 시를 입력하세요 : ");
 	scanf(" %[^\n]s", &adrSi);
-	student->adrSi = adrSi;
-	// 예외처리 함수 호출하기. -> adrSi가 문자(열)인지 아닌지.
+	int cntSi = 0;
+	for (int i = 0; adrSi[cntSi] != '\0'; i++) {
+		cntSi++;
+	}
+	student->adrSi = (char*)malloc(cntSi + 1); // 주소-시에 동적할당하기.
+	for (int i = 0; i <= cntSi; i++) { // 동적할당한 student의 adrSi에 이곳 adrSi의 각각의 문자를 할당함.
+		student->adrSi[i] = adrSi[i];
+	}
 
 	printf("[SYSTEM]추가할 학생의 거주하고 있는 구를 입력하세요 : ");
 	scanf(" %[^\n]s", &adrGu);
-	student->adrGu = adrGu;
-	// 예외처리 함수 호출하기. -> adrGu가 문자(열)인지 아닌지.
+	int cntGu = 0;
+	for (int i = 0; adrGu[cntGu] != '\0'; i++) {
+		cntGu++;
+	}
+	student->adrGu = (char*)malloc(cntGu + 1); // 주소-구에 동적할당하기.
+	for (int i = 0; i <= cntGu; i++) { // 동적할당한 student의 adrGu에 이곳 adrGu의 각각의 문자를 할당함.
+		student->adrGu[i] = adrGu[i];
+	}
 
 	printf("[SYSTEM]추가할 학생의 성적을 입력하세요 : ");
-	scanf(" %lf", &grade);
-	student->grade = grade;
-	// 예외처리 함수 호출하기. -> grade가 숫자인지 아닌지.
-	
-	student->next = NULL;
-	stdList->top = student;
+	scanf("%s", text);
+	for (int i = 0; text[cntGrade] != '\0'; i++) {
+		cntGrade++;
+	}
+	while (!Getting_IsitNumber(text, cntGrade)) {
+		printf("숫자만 입력하세요 : ");
+		scanf("%s", text);
+		cntGrade = 0;
+		for (int i = 0; text[cntGrade] != '\0'; i++) {
+			cntGrade++;
+		}
+	}
+	student->grade = Changing_StringToInt(text);
 
-	printf("%d, %s, %s, %s, %s, %s, %.1lf\n\n", student->number, student->name, student->adrCountry, student->adrDo, student->adrSi, student->adrGu, student->grade);
+	if (stdList->head == NULL) { // StudnetList에 Student가 없을 때만 실행 => 첫 번째 Student 추가시만 실행.
+		stdList->head = student;
+		stdList->tail = student;
+	}else{
+		stdList->tail->next = student;
+		stdList->tail = student;
+	}
+
+	stdList->size++;
+}
+
+void Printing_StudentList(StudentList* stdList) {
+	
+	Student* current = stdList->head;
+
+	for (int i = 0; i < stdList->size; i++) {
+		printf("[%d번째] 번호 : %d | 이름 :  %s | 국가 : %s | 도 : %s | 시 : %s | 구 : %s | 등급 : %d |\n", i, current->number, current->name, current->adrCountry, current->adrDo, current->adrSi, current->adrGu, current->grade);
+		current = current->next;
+	}
+
 }
