45245c2a72c54a79fac18b5d66449cc2d9e3bb3d
Kinesis <lordkinesis@naver.com>
2024-07-11 22:04:59 +0900

    Update: HW_001.c
    임시 저장.
    집 가서 마저 작업하기.

--- Projects/C_Language/HomeWork/HW_20240711/HW_001.c
+++ Projects/C_Language/HomeWork/HW_20240711/HW_001.c
@@ -2,8 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 주소 구조체
+typedef struct _Address {
+	char* country[20]; // 국가
+	char* state[20]; // 도
+	char* city[20]; // 시
+	char* town[20]; // 구
+}Address;
+// 학생 구조체
+typedef struct _Student {
+	int* number;
+	char* name;
+	Address* adr;
+	int* list;
+}Student;
+
+void Initializing(Student* std);
+
 int main() {
 
+	Student* student = (Student*)malloc(sizeof(student));
+
+	Initializing(student);
+
+	if (student->adr == NULL) {
+		printf("NULL상태.");
+	}
+
+	free(student);
 
 	return 0;
+}
+
+void Initializing(Student* std) {
+
+	std->number = 0;
+	std->name = " ";
+	std->adr = NULL;
+	std->list = NULL;
 }
