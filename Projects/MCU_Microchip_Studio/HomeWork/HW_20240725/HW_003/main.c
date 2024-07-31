49d2de9b4e137653db53dc427c2b826309bdce2f
Kinesis <lordkinesis@naver.com>
2024-07-31 10:47:20 +0900

    :wrench: Create Microchip Studio Files

---
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240725/HW_003/main.c
@@ -0,0 +1,26 @@
+/*
+ * HW_003.c
+ *
+ * Created: 2024-07-31 오전 10:28:26
+ * Author : lordk
+ * SRS:
+ * 모든 SRS(명세서)는 과제보고서와 작업일지(개인)에 기록함.
+ * 1. 0.1s 마다 LED 2진 카운터
+ * [Ext Interrupt]
+ * 2. INT0 발생시 LED 3개씩 우측 이동 X 2
+ * 3. INT1 발생시 LED 3개씩 좌측 이동 X 2
+ * 4.  INT2 발생시 LED1개 좌측 이동 후 우측 이동
+ * 5. INT3 발생시 2진 카운터 초기화
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
