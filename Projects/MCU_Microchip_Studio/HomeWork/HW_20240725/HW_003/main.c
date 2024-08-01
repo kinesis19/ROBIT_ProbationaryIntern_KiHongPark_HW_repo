ac5aebb67cc019dee29a0e5f652bb3656bd6fff9
Kinesis <lordkinesis@naver.com>
2024-08-01 11:44:37 +0900

    :hammer: Update main.c
    임시 저장.
    기능 1 구현 중. 순차 점등 방식은 구현 완료 함. 조건에 따라 max idx까지 반복해서 출력하고, 다시 처음으로 돌아가 반복해서 출력하는 로직을 설계해야 함. 지금은 왜 한 번 밖에 안 될까 싶었으나, buff 초기화를 안 해 줘서 그런거 였음. 커밋하고 수정해야 할 듯.

--- Projects/MCU_Microchip_Studio/HomeWork/HW_20240725/HW_003/main.c
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240725/HW_003/main.c
@@ -7,20 +7,44 @@
  * 모든 SRS(명세서)는 과제보고서와 작업일지(개인)에 기록함.
  * 1. 0.1s 마다 LED 2진 카운터
  * [Ext Interrupt]
- * 2. INT0 발생시 LED 3개씩 우측 이동 X 2
+ * 2. INT0 s발생시 LED 3개씩 우측 이동 X 2
  * 3. INT1 발생시 LED 3개씩 좌측 이동 X 2
  * 4.  INT2 발생시 LED1개 좌측 이동 후 우측 이동
  * 5. INT3 발생시 2진 카운터 초기화
  */ 
 
+#define F_CPU 16000000
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
+int main(void){
+	// 레지스터 선언하기.
+	DDRA = 0xFF;
+	PORTA = 0xFF;
+	
+	// 변수 선언하기.
+	unsigned char buff = 0x01; // 1111 1110 -> Active-Low 방식일 때는 1111 1110으로 표기.
+	unsigned int ledIdx = 0; // 2진 카운터를 위한 led의 index 현재값.
+	unsigned int ledMaxIdx = 0; // 2진 카운터를 위한 led index의 최대값.
+	
+	
+    while (1) {
+		PORTA = buff;
+		// 현재 led가 maxLed의 위치와 같을 때,
+		if(ledIdx == ledMaxIdx){
+			ledIdx = 0;
+			if(ledIdx == 7){ // 7번째 LED까지 켜졌으면, 다시 0번째 Led로 돌아가기
+				ledMaxIdx = 0;
+			}else{
+				ledMaxIdx++;
+			}
+		}else{
+			buff = buff << 1;
+			_delay_ms(100);
+		}
+		
     }
 }
 
