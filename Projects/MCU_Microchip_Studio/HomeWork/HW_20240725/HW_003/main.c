c9da923a44258d24f74eabd66d2d68fffc07864e
Kinesis <lordkinesis@naver.com>
2024-08-01 14:40:30 +0900

    :hammer: Update main.c
    아......... 2진 카운터 개념은 알겠는데, 구현 방법을 모르겠다!!!!!!!!!!!!!!!!!!!!!!!!!!!
    
    Maximum Index까지 띄우고, 0번째 index부터 ~ Maximum index - 1까지 채우고, 또 Maximum index - 2까지 채워야 하는데 어떻게 하는 거야!!!!!!!!!!
    
    OR( | ) 연산자 사용하면 될 것 같으면서도 안 되는 이것은 어떻게 해야 하는거야!!!!!
    
    이틀에 한 번 자고 있는 데, 피곤해서 구현이 안 되는거야 아니면 실력이 부족해서 그런거야!!!!!!!
    
    Boooooooooooooooth!
    
    둘 다 같아!!!!!!!!!!!!!

--- Projects/MCU_Microchip_Studio/HomeWork/HW_20240725/HW_003/main.c
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240725/HW_003/main.c
@@ -7,7 +7,7 @@
  * 모든 SRS(명세서)는 과제보고서와 작업일지(개인)에 기록함.
  * 1. 0.1s 마다 LED 2진 카운터
  * [Ext Interrupt]
- * 2. INT0 s발생시 LED 3개씩 우측 이동 X 2
+ * 2. INT0 발생시 LED 3개씩 우측 이동 X 2
  * 3. INT1 발생시 LED 3개씩 좌측 이동 X 2
  * 4.  INT2 발생시 LED1개 좌측 이동 후 우측 이동
  * 5. INT3 발생시 2진 카운터 초기화
@@ -25,26 +25,39 @@ int main(void){
 	PORTA = 0xFF;
 	
 	// 변수 선언하기.
-	unsigned char buff = 0x01; // 1111 1110 -> Active-Low 방식일 때는 1111 1110으로 표기.
+	unsigned char buff = 0b11111110; // 1111 1110 -> Active-Low 방식일 때는 1111 1110으로 표기.
 	unsigned int ledIdx = 0; // 2진 카운터를 위한 led의 index 현재값.
 	unsigned int ledMaxIdx = 0; // 2진 카운터를 위한 led index의 최대값.
 	
+	// #Debugging:
+	//buff = buff << 1;
+	//PORTA  = buff;
+	//
+	//_delay_ms(1000);
+	//
+	//buff = buff << 1;
+	//PORTA  = buff;
+	
+	// #End
+	
+	
 	
     while (1) {
-		PORTA = buff;
-		// 현재 led가 maxLed의 위치와 같을 때,
-		if(ledIdx == ledMaxIdx){
-			ledIdx = 0;
-			if(ledIdx == 7){ // 7번째 LED까지 켜졌으면, 다시 0번째 Led로 돌아가기
-				ledMaxIdx = 0;
-			}else{
-				ledMaxIdx++;
-			}
-		}else{
-			buff = buff << 1;
+		
+		for(int i = 0; i < ledMaxIdx; i++){
 			_delay_ms(100);
+			buff = buff << 1;
+			PORTA = buff;
 		}
 		
+		ledMaxIdx++;
+		buff = 0xFF;
+		if(ledMaxIdx > 4){
+			ledMaxIdx = 0;
+		}
+		
+		_delay_ms(100);
+		PORTA = buff;
     }
 }
 
