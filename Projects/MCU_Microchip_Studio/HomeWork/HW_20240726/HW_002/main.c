2b719a15dbd0e448700127daf09db41ac542de8c
Kinesis <lordkinesis@naver.com>
2024-08-01 14:55:24 +0900

    :hammer: Create main.c
    LCD 과제 2번 파일을 생성함.

---
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240726/HW_002/main.c
@@ -0,0 +1,18 @@
+/*
+ * HW_002.c
+ *
+ * Created: 2024-08-01 오후 2:50:31
+ * Author : lordk
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
