fb0d46816e3cb3cf419312188fb10631e1f7d670
Kinesis <lordkinesis@naver.com>
2024-08-02 17:15:54 +0900

    :hammer: Update Temp feature in HW_003.c
    변수 numA, numB, numC에 대하여 증감한 결과가 반영이 안 되는 현상이 발생 됨. 으어!!!!!!!!!!!!!!!!!!!!! 왜 이러지.
    
    <Test Case#1>
    0번째 스위치 입력 -> 0번째 스위치 입력 조건에는 변수 numA의 값이 1 증가하여 2가 출력됨. -> 3번째 스위치를 입력하여 numA의 값을 출력하였으나 2가 아닌 1이 출력됨.

--- Projects/MCU_Microchip_Studio/HomeWork/HW_20240726/HW_003/HW_003.c
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240726/HW_003/HW_003.c
@@ -13,13 +13,90 @@
  * 4. 네번째 스위치를 누르면 연산을 하여 값을 LCD에 띄운다
  */ 
 
+#define F_CPU 16000000
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include "LCD_Text.h"
 
-int main(void)
-{
+
+int main(void) {
+	
+	// 레지스터 선언하기
+	DDRD = 0x00; // 입력모드
+	
+	EIMSK = 0b00001111;
+	EICRA = 0xFF; // 0b11111111와 동일함.
+	
+	// 변수 선언하기
+	unsigned int numA = 1, numB = 1, numC = 0;
+	unsigned int operator = 0;
+	
+	sei();
+	
+	lcdInit();	
+	lcdClear();
+	
 	
     while (1) {
+		if(!(PIND & (1 << PIND0))) {
+			numA++;
+			
+			lcdString(0, 0, "NUM1");
+			lcdNumber(1, 0, numA);
+			
+			_delay_ms(10000);
+			
+		}else if(!(PIND & (1 << PIND1))) {
+			if(operator < 3){
+				operator++;
+			}else{
+				operator--;
+			}		
+		
+		}else if(!(PIND & (1 << PIND2))) {
+			numB++;
+			lcdString(0, 0, "NUM2");
+			lcdNumber(1, 0, numB);
+			
+			_delay_ms(10000);
+		}else if(!(PIND & (1 << PIND3))) {
+			
+			//char resultLine[16] = "";
+			//char tempStr1[16] = "";
+			//char tempStr2[16] = "";
+			//char tempStr3[16] = "";
+			//char tempStr4[16] = "";
+			//char tempStr5[16] = "";
+			//
+			//if(operator == 0){
+				//numC = numA + numB;
+				//
+				//sprintf(tempStr1, "%d", numA);
+				//strcpy(tempStr2, "+");
+				//sprintf(tempStr3, "%d", numB);
+				//strcpy(tempStr4, "=");
+				//sprintf(tempStr5, "%d", numC);
+				//strcat(resultLine, tempStr1);
+				//strcat(resultLine, tempStr2);
+				//strcat(resultLine, tempStr3);
+				//strcat(resultLine, tempStr4);
+				//strcat(resultLine, tempStr5);
+				//
+			//}else if(operator == 1){
+				//numC = numA - numB;
+			//}else if(operator == 2){
+				//numC = numA * numB;
+			//}else if(operator == 3){
+				//numC = numA / numB;
+			//}
+			
+			//lcdString(0, 0, resultLine);
+			lcdString(0, 0, "HEEEEEEEY!");
+			lcdNumber(1, 0, numA);
+		}
 		
     }
+	
 }
-
