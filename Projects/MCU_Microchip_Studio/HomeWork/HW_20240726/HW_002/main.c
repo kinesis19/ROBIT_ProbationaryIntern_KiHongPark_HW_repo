8cf9b15da2972ce74edcef1804032510389cccb9
Kinesis <lordkinesis@naver.com>
2024-08-02 09:35:52 +0900

    :sparkles: Add feature1, feature2, feature4 to main.c
    아직 feature3 관련 기능은 구현하지 못 함. 파일명부터 변경하고, feature3(전압 값 표시) 구현해야 함.

--- Projects/MCU_Microchip_Studio/HomeWork/HW_20240726/HW_002/main.c
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240726/HW_002/main.c
@@ -13,17 +13,20 @@
 
 #define F_CPU 16000000
 #include <avr/io.h>
-#include "LCD_Text.h"
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "LCD_Text.h"
 
 int main(void){
 	DDRA = 0xFF;
-	
 	DDRF = 0x00;
+	
+	PORTA = 0xFF;
+	
 	ADMUX = 0x40;
 	ADCSRA = 0x87;
-	SREG = 0x80;
+	//SREG = 0x80; // -> 에러 남.
+	sei(); // SREG 대신 sei() 함수 사용함.
 	
 	lcdInit();
 	lcdClear();
@@ -32,25 +35,48 @@ int main(void){
 	
 	while(1){
 		
-		//unsigned int adcValue = 0;
-		//unsigned char channel = 0x00;
-		//
-		//ADMUX = 0x40 | channel;
-		//ADCSRA |= 0x40;
-		//
-		//while(ADCSRA&0x10 == 0){
-			//adcValue = ADC;
-			////lcdNumber(1, 0, adcValue);
-		//}
-		//
-		//lcdString(0, 0, "19th_KH");
-		//lcdNumber(1, 0, adcValue);
-		//
-		//_delay_ms(100);
-		//
-		//
+		unsigned int adcValue = 0;
+		unsigned char channel = 0x00;
+		
+		ADMUX = 0x40 | channel;
+		ADCSRA |= 0x40;
 		
 		
+		while((ADCSRA&0x10) == 0){
+			adcValue = ADC;
+			int adcValueVoltage = (5 / 1024) * adcValue;
+			// <2번 기능>
+			lcdNumber(1, 0, adcValue); 
+			// <3번 기능>
+			lcdNumber(1, 6, adcValueVoltage); // adcVaulue는 네 칸을 확보하므로, 가독성을 위해 한 칸 공백 후 6칸부터 전압을 표시함.
+			_delay_ms(100); // lcd 초기화를 진행하여 값의 변화에 따라 ldc에 출력함.
+			lcdClear();
+			
+			// <1번 기능>
+			if(adcValue < (1023 / 8) * 1){
+				PORTA = 0b11111110;
+			}else if(adcValue < (1023 / 8) * 2){
+				PORTA = 0b11111101;
+			}else if(adcValue < (1023 / 8) * 3){
+				PORTA = 0b11111011;
+			}else if(adcValue < (1023 / 8) * 4){
+				PORTA = 0b11110111;
+			}else if(adcValue < (1023 / 8) * 5){
+				PORTA = 0b11101111;
+			}else if(adcValue < (1023 / 8) * 6){
+				PORTA = 0b11011111;
+			}else if(adcValue < (1023 / 8) * 7){
+				PORTA = 0b10111111;
+			}else if(adcValue < (1023 / 8) * 8){
+				PORTA = 0b01111111;
+			}
+			
+			
+		}
+		
+		lcdString(0, 0, "19th_KH");
+		
+		_delay_ms(50);
 	}
 	
 }
