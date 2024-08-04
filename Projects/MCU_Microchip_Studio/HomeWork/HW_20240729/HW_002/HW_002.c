af84ef5cd24b2401f8c2dce6e1ae2a77e85132e3
Kinesis <lordkinesis@naver.com>
2024-08-04 21:51:11 +0900

    :sparkles: Add feature1 to HW_002.c

--- Projects/MCU_Microchip_Studio/HomeWork/HW_20240729/HW_002/HW_002.c
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240729/HW_002/HW_002.c
@@ -15,13 +15,80 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include <math.h>
 #include "LCD_Text.h"
+
+//unsigned char data, receive;
+//
+//ISR(USART0_RX_vect){
+	//receive = 1;
+	//data = UDR0;
+//}
+//
+//
+//ISR(USART0_UDRE_vect){
+	//if(receive == 1){
+		//UDR0 = '_';
+		//while((UCSR0A&0x20) == 0);
+		//UDR0 = 'o';
+		//while((UCSR0A&0x20 == 0));
+		//UDR0 = 'k';
+		//
+		//receive = 0;
+	//}
+	//UCSR0B = 0x98;
+//}
+
 
 int main(void) {
 	
+	DDRA = 0xFF;
+	DDRF = 0x00;
+	DDRB = 0xFF;
+	
+	PORTA = 0xFF;
+	PORTB = 0x00;
+	
+	ADMUX = 0x40;
+	ADCSRA = 0x87;
+	
+	//UCSR0A = 0x00;
+	//UCSR0B = 0x98;
+	//UCSR0C = 0x06;
+	//UBRR0H = 0x00;
+	//UBRR0L = 0xCF;
+	
+	
+	sei();
+	
+	lcdInit();
+	lcdClear();
+	
     while (1) {
+		
+		//if(data == 'y'){
+			//PORTB = 0xFF;
+			//UCSR0B = 0xB8;
+		//}
+		//if(data = 'n'){
+			//PORTB = 0x00;
+			//UCSR0B = 0xB8;
+		//}
+		
+		unsigned int adcValue = 0;
+	    unsigned char channel = 0x00;
+		
+		ADMUX = 0x40 | channel;
+		ADCSRA |= 0x40;
+		
+		while((ADCSRA&0x10) == 0) {
+			adcValue = ADC;
+			
+		}
+		
+		lcdClear();
+		lcdString(0, 0, "ADC Value : ");
+		lcdNumber(0, 12, adcValue);
+		_delay_ms(100);
 		
     }
 }
-
