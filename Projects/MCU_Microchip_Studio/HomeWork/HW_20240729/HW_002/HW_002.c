8bcdc1ced2013ae173b088425d2230ddbf9e2b1f
Kinesis <lordkinesis@naver.com>
2024-08-05 23:48:06 +0900

    :sparkles: Add feature2 to HW_002.c

--- Projects/MCU_Microchip_Studio/HomeWork/HW_20240729/HW_002/HW_002.c
+++ Projects/MCU_Microchip_Studio/HomeWork/HW_20240729/HW_002/HW_002.c
@@ -15,12 +15,68 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 #include "LCD_Text.h"
 
+void Initializing_Register(void);
 unsigned char Uart_Getch(void);
 void Uart_Putch(unsigned char PutData);
+void Uart_PutNum(unsigned int PutData);
 
 int main(void) {
+	
+	int cntOutput = 0;
+	
+	Initializing_Register();
+	
+    while (1) {
+				
+		unsigned int adcValue = 0;
+	    unsigned char channel = 0x00;
+		
+		ADMUX = 0x40 | channel;
+		ADCSRA |= 0x40;
+		
+		while((ADCSRA&0x10) == 0) {
+			adcValue = ADC;
+		}
+		
+		// LCD: Show
+		lcdClear();
+		lcdString(0, 0, "ADC Value : ");
+		lcdNumber(0, 12, adcValue);
+		
+		// UART Communication: to PC
+		Uart_Putch('A');
+		Uart_Putch('D');
+		Uart_Putch('C');
+		Uart_Putch(':');
+		Uart_PutNum(adcValue);
+		
+		if(cntOutput == 20){
+			Uart_Putch('\r'); // MS-DOS에서 개행은 캐리지 리턴 진행 후 -> 개행을 진행함.
+			Uart_Putch('\n');
+			cntOutput = 0;
+		}else{
+			Uart_Putch(' ');
+			cntOutput++;
+		}
+		
+		_delay_ms(500);
+				
+    }
+}
+void Initializing_Register(void){
+	
+	DDRA = 0xFF;
+	DDRF = 0x00;
+	DDRB = 0xFF;
+	
+	PORTA = 0xFF;
+	PORTB = 0x00;
+	
+	ADMUX = 0x40;
+	ADCSRA = 0x87;
 	
 	UBRR0L = 103;
 	UBRR0H = 0;
@@ -32,62 +88,8 @@ int main(void) {
 	
 	sei();
 	
-	while (1) {	
-		//char recvData = Uart_Getch();
-		Uart_Putch('H');
-		_delay_ms(1000);
-	}	
-	
-	//DDRA = 0xFF;
-	//DDRF = 0x00;
-	//DDRB = 0xFF;
-	//
-	//PORTA = 0xFF;
-	//PORTB = 0x00;
-	//
-	//ADMUX = 0x40;
-	//ADCSRA = 0x87;
-	//
-	////UCSR0A = 0x00;
-	////UCSR0B = 0x98;
-	////UCSR0C = 0x06;
-	////UBRR0H = 0x00;
-	////UBRR0L = 0xCF;
-	//
-	//
-	//sei();
-	//
-	//lcdInit();
-	//lcdClear();
-	//
-    //while (1) {
-		//
-		////if(data == 'y'){
-			////PORTB = 0xFF;
-			////UCSR0B = 0xB8;
-		////}
-		////if(data = 'n'){
-			////PORTB = 0x00;
-			////UCSR0B = 0xB8;
-		////}
-		//
-		//unsigned int adcValue = 0;
-	    //unsigned char channel = 0x00;
-		//
-		//ADMUX = 0x40 | channel;
-		//ADCSRA |= 0x40;
-		//
-		//while((ADCSRA&0x10) == 0) {
-			//adcValue = ADC;
-			//
-		//}
-		//
-		//lcdClear();
-		//lcdString(0, 0, "ADC Value : ");
-		//lcdNumber(0, 12, adcValue);
-		//_delay_ms(100);
-		//
-    //}
+	lcdInit();
+	lcdClear();
 }
 
 
@@ -99,4 +101,18 @@ unsigned char Uart_Getch(void){
 void Uart_Putch(unsigned char PutData){
 	while(!(UCSR0A & (1 << UDRE0)));
 	UDR0 = PutData;
+}
+
+void Uart_PutNum(unsigned int PutData){
+	
+	// 버퍼 확보하기.
+    char bufferNum[10]; 
+	
+	// 숫자를 문자열로 변환하기.
+    snprintf(bufferNum, sizeof(bufferNum), "%u", PutData); 
+	
+    // 숫자가 저장된 문자열 배열 bufferNum에 NULL 값이 없을 때까지 반복하기.
+    for (char *ptr = bufferNum; *ptr != '\0'; ptr++) { 
+        Uart_Putch(*ptr);
+    }
 }
