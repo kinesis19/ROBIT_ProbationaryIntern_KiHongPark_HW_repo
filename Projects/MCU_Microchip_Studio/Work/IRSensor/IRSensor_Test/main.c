908167901bc4ef706c3137c5c4e27b54f00de50c
Kinesis <lordkinesis@naver.com>
2024-08-08 21:51:01 +0900

    :hammer: Temp Update in main.c
    임시 저장.
    adc를 회로에 장착된 가변 저항 값 말고, IR 센서의 adc 값을 불러오는 방법에 대하여 먼저 조사하기. 조사한 내용을 바탕으로 이해하고 다시 리팩토링 하기.

--- Projects/MCU_Microchip_Studio/Work/IRSensor/IRSensor_Test/main.c
+++ Projects/MCU_Microchip_Studio/Work/IRSensor/IRSensor_Test/main.c
@@ -3,6 +3,10 @@
  *
  * Created: 2024-08-08 오후 4:47:43
  * Author : lordk
+ * Solution :
+ * 기존 나의 풀이 방법 : IR Sensor로부터 직접 값을 입력 받아 비교하기 -> 안 됨.
+ * IR Sensor는 ADC 값과 연관 되어 있다는 사실을 알게 됨.
+ * 1. IR Sensor의 ADC값을 받아와 작동 시키기.
  */ 
 
 #define F_CPU 16000000
@@ -13,14 +17,40 @@
 #include <math.h>
 #include "LCD_Text.h"
 
+void Detecting(void);
+
 int main(void) {
+	
+	DDRA = 0xFF;
+	DDRF = 0x00; // Input Mode.
+	
+	ADMUX = 0x40;
+	ADCSRA = 0x87;
+	
+	sei();
 	
 	lcdInit();
 	lcdClear();
 	
-    while (1) {
-		lcdString(0, 0, "aa");
-		_delay_ms(100);
-    }
+	while(1){
+		
+		unsigned int adcValue = 0;
+		unsigned char channel = 0x00;
+		
+		ADMUX = 0x40 | channel;
+		ADCSRA |= 0x40;
+		
+		while((ADCSRA&0x10) == 0){ // ADC의 Interrupt가 없을 때,
+			adcValue = ADC;
+			
+			// <2번 기능>
+			lcdNumber(1, 0, adcValue);	
+		}
+		
+		_delay_ms(50);
+		
+		lcdClear();
+	}
+		
+	
 }
-
