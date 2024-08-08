b69dd84229d591f075cfc6ff6fd7897345e4004f
Kinesis <lordkinesis@naver.com>
2024-08-08 23:08:02 +0900

    :wrench: Add VSCode
    VSCode에서 mi

---
+++ Projects/MCU_Microchip_Studio/Work/Test/main.c
@@ -0,0 +1,16 @@
+/** 
+ * @file main.c
+ * @author lordk
+ * @date 2024-08-08
+ * @brief Main function 
+ */
+
+#include <avr/io.h>
+
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+
+};
