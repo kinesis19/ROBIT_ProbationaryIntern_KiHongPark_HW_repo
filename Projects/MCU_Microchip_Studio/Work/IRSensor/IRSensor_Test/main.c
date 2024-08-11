52caa16443d0973d9d231926b64ca745c1363a68
Kinesis <lordkinesis@naver.com>
2024-08-12 04:31:41 +0900

    :hammer: Update: IRSensor_Test
    Normalization 구하기 완료

--- Projects/MCU_Microchip_Studio/Work/IRSensor/IRSensor_Test/main.c
+++ Projects/MCU_Microchip_Studio/Work/IRSensor/IRSensor_Test/main.c
@@ -24,8 +24,7 @@ void Detecting(void);
 unsigned int irSensorList[6] = {0, }; // IR Sensor의 ADC 값 저장 배열 선언하기.	
 unsigned int irSensorListMax[6] = {0, };
 unsigned int irSensorListMin[6] = {1023, 1023, 1023, 1023, 1023, 1023};
-unsigned int irSensorListNormalization[6] = {0, };
-bool isIRSensorInit = false;	
+double irSensorListNormalization[6] = {0, };
 	
 
 int main(void) {
@@ -49,66 +48,40 @@ int main(void) {
 		for(int i = 0; i < 6; i++){
 			ADMUX = (ADMUX & 0xF0) | idx;
 			ADCSRA |= (1 << ADSC); 
-			while(ADCSRA & (1 << ADIF));
+			while(!(ADCSRA & (1 << ADIF)));
 			
-			irSensorList[i] = ADC;			
+			irSensorList[i] = ADC;
+			
+			//lcdNumber(1, 0, irSensorList[i]);
 			idx++;
 		}
 		
-		lcdNumber(1, 0, irSensorList[0]);
-		// IR Sensor의 MIN, MAX 할당하기
-		if(isIRSensorInit == false){
-			
-			for(int i = 0; i < 6; i++){
-				if(irSensorList[i] < irSensorListMin[i]){
-					irSensorListMin[i] = irSensorList[i];
-				}
-				if(irSensorList[i] > irSensorListMax[i]){
-					irSensorListMax[i] = irSensorList[i];
-				}
+		// IR Sensor의 MIN, MAX 할당하기.
+		for(int i = 0; i < 6; i++){
+			if(irSensorList[i] < irSensorListMin[i]){
+				irSensorListMin[i] = irSensorList[i];
 			}
-			
-			isIRSensorInit = true;
-		}else{
-			for(int i = 0; i < 6; i++){
-				
-				irSensorListNormalization[i] = ((irSensorList[i] - irSensorListMin[i]) / (irSensorListMax[i] - irSensorListMin[i])) * 100;
-				
+			if(irSensorList[i] > irSensorListMax[i]){
+				irSensorListMax[i] = irSensorList[i];
 			}
 		}
 		
-		lcdNumber(0, 0, irSensorListMin[0]);
-		lcdNumber(0, 4, irSensorListMax[0]);
-		//lcdNumber(1, 0, irSensorList[0]);
-		//lcdNumber(1, 4, irSensorListNormalization[0]);
 		
-		
-		//lcdNumber(0, 0, irSensorList[1]); // PF2
-		//lcdNumber(0, 4, irSensorList[2]); // PF3
-		//lcdNumber(0, 8, irSensorList[3]); // PF4
-		//lcdNumber(1, 0, irSensorList[4]); // PF5
-		//lcdNumber(1, 4, irSensorList[5]); // PF6
-		//lcdNumber(1, 8, irSensorList[0]); // PF7
-		
-		
-		//lcdNumber(0, 0, irSensorListNormalization[1]); // PF2
-		//lcdNumber(0, 4, irSensorListNormalization[2]); // PF3
-		//lcdNumber(0, 8, irSensorListNormalization[3]); // PF4
-		//lcdNumber(1, 0, irSensorListNormalization[4]); // PF5
-		//lcdNumber(1, 4, irSensorListNormalization[5]); // PF6
-		//lcdNumber(1, 8, irSensorListNormalization[0]); // PF7
-		_delay_ms(100);
-		
-		
-		// 저장된 IR Sensor0부터 ~ 5까지의 ADC값 출력하기
-		//for(int j = 0; j < 6; j++){
-			//char label[4];
-			//sprintf(label, "IR%d", j); // label에 몇 번째 IR Sensor인지 형식에 맞춰 저장하기.
-			//lcdString(0, 0, label);
-			//lcdNumber(1, 0, irSensorList[j]);
-			//_delay_ms(500);
-			//lcdClear();
-		//}
-		
+		for(int i = 0; i < 6; i++){
+			
+			irSensorListNormalization[i] = ((  (double)irSensorList[i] - irSensorListMin[i]) / (irSensorListMax[i] - irSensorListMin[i])) * 100;
+			
+			lcdNumber(0, 0, irSensorListNormalization[0]); // PF2 - IR2
+			lcdNumber(0, 4, irSensorListNormalization[1]); // PF3 - IR1
+			lcdNumber(0, 8, irSensorListNormalization[2]); // PF4 - IR0
+			lcdNumber(1, 0, irSensorListNormalization[3]); // PF5 - IR5
+			lcdNumber(1, 4, irSensorListNormalization[4]); // PF6 - IR4
+			lcdNumber(1, 8, irSensorListNormalization[5]); // PF7 - IR3
+			
+			_delay_ms(500);
+			
+			lcdClear();
+			
+		}		
 	}
 }
