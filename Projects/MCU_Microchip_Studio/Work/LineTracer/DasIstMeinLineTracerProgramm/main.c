34bee5f8cb985dcc8e363e64d478b29a51766863
Kinesis <lordkinesis@naver.com>
2024-08-11 20:48:58 +0900

    :hammer: Create Line-Tracer main.c

---
+++ Projects/MCU_Microchip_Studio/Work/LineTracer/DasIstMeinLineTracerProgramm/main.c
@@ -0,0 +1,18 @@
+/*
+ * DasIstMeinLineTracerProgramm.c
+ *
+ * Created: 2024-08-11 오후 8:47:59
+ * Author : lordk
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
