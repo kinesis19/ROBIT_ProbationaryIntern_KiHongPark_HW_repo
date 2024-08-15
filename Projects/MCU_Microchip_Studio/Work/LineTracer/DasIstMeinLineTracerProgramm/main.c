10921b33fe73b654d74e4e07376c5037c91e522f
Kinesis <lordkinesis@naver.com>
2024-08-15 15:11:22 +0900

    :hammer: All mode in LineTracer
    Mode2가 정상 작동 함.
    
    :warning: 단, psd[1]의 값이 이상함.
    -> 멈추면 안 되는 곳에서 psd값이 충족 되어 버려 바로 map2로 변하해버림.

--- Projects/MCU_Microchip_Studio/Work/LineTracer/DasIstMeinLineTracerProgramm/main.c
+++ Projects/MCU_Microchip_Studio/Work/LineTracer/DasIstMeinLineTracerProgramm/main.c
@@ -166,6 +166,38 @@ void Detecting(void){
 // 완성된 모드 기능
 void Motor_Control_Mode1(void){
 	
+	if(systemMode == 0){
+		// -----[주행 기능]-----
+		// -----[메인 주행 기능]-----
+		if(irSensorListNormalization[3] < 50){
+			Motor_Turning_Left();
+			PORTA = 0b00111111;
+		}else if(irSensorListNormalization[3] > 50){
+			PORTA = 0b11111100;
+			Motor_Turning_Right();
+		}
+		
+		// -----[stage-3(바코드) Exception Handling]-----
+		if(irSensorListNormalization[0] < 50){ // IR2가 검은색에 가까울  때
+			PORTA = 0b01111111;
+			Motor_Turning_Left();
+		}
+		// 바코드에서 마지막 전체 흰 줄일 때,
+		if(irSensorListNormalization[1] < 50 && irSensorListNormalization[2] < 50){
+			PORTA = 0b01111111;
+			for(int i = 0; i < 5; i++){
+				PORTA = 0b10101010;
+				Motor_Turning_Left();
+			}
+			
+			// Map2 입구 도착 여부 체크하기
+			if((psdSnesorList[0] < 70 && 250 < psdSnesorList[1]) && IRSensorCheckerAllBlack() == 1){ // Map2 입구 도착 조건에 충족 할 때,
+				systemMode = 1; // systemMode를 Map2에 알맞게 설정하기.
+			}
+		}
+	}else if(systemMode == 1){ // Map2에서의 systemMode
+		Motor_Moving_Stop();
+	}
 }
 
 // 주행 실험 모드
@@ -190,7 +222,7 @@ void Motor_Control_Mode2(void){
 		// 바코드에서 마지막 전체 흰 줄일 때,
 		if(irSensorListNormalization[1] < 50 && irSensorListNormalization[2] < 50){
 			PORTA = 0b01111111;
-			for(int i = 0; i < 4; i++){
+			for(int i = 0; i < 6; i++){
 				PORTA = 0b10101010;
 				Motor_Turning_Left();
 			}
@@ -201,7 +233,7 @@ void Motor_Control_Mode2(void){
 			}
 		}
 	}else if(systemMode == 1){ // Map2에서의 systemMode
-		Motor_Moving_Forward();
+		Motor_Moving_Stop();
 	}
 }
 
@@ -238,7 +270,7 @@ void Motor_Control_Mode3(void){
 			}
 		}
 	}else if(systemMode == 1){ // Map2에서의 systemMode
-		Motor_Moving_Forward();
+		Motor_Moving_Stop();
 	}
 	
 }
