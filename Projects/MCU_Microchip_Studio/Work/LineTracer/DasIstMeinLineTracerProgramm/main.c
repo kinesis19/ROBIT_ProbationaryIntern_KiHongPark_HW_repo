56b934fc39cf0d8fd3033b5f1f6e61d0509628de
Kinesis <lordkinesis@naver.com>
2024-08-16 16:35:03 +0900

    :recycle: Update: Refactoring All Mode
    맵의 변화로 인하여 전체 리팩토링 진행 후 -> 최대한 간결하게 코드를 구성할 계획임.

--- Projects/MCU_Microchip_Studio/Work/LineTracer/DasIstMeinLineTracerProgramm/main.c
+++ Projects/MCU_Microchip_Studio/Work/LineTracer/DasIstMeinLineTracerProgramm/main.c
@@ -178,6 +178,7 @@ void Detecting(void){
 // 완성된 모드 기능
 void Motor_Control_Mode1(void){
 	
+	
 }
 
 // 주행 실험 모드
@@ -187,75 +188,19 @@ void Motor_Control_Mode2(void){
 
 
 void Motor_Control_Mode3(void){
-	
-	
 	if(systemMode == 0){
 		// -----[주행 기능]-----
 		// -----[메인 주행 기능]-----
 		if(irSensorListNormalization[3] < 50){
-			Motor_Turning_Left();
-			PORTA = 0b00111111;
-		}else if(irSensorListNormalization[3] > 50){
 			PORTA = 0b11111100;
 			Motor_Turning_Right();
-		}
-		
-		// -----[stage-3(바코드) Exception Handling]-----
-		if(irSensorListNormalization[0] < 50){ // IR2가 검은색에 가까울  때
-			PORTA = 0b01111111;
+		}else if(irSensorListNormalization[3] > 50){
+			PORTA = 0b00111111;
 			Motor_Turning_Left();
-		}
-		// 바코드에서 마지막 전체 흰 줄일 때,
-		if(irSensorListNormalization[1] < 50 && irSensorListNormalization[2] < 50){
-			PORTA = 0b01111111;
-			for(int i = 0; i < 6; i++){
-				PORTA = 0b10101010;
-				Motor_Turning_Left();
-			}
-			
-			// Map2 입구 도착 여부 체크하기
-			if((psdSnesorList[0] < 70 && 250 < psdSnesorList[1]) && IRSensorCheckerAllBlack() == 1){ // Map2 입구 도착 조건에 충족 할 때,
-				
-				if(stage3CntTurnLeft < 3){
-					PORTA = 0b11110000;
-					Motor_Turning_Left();
-				}
-				if(stage3CntMoveForward < 3){
-					PORTA = 0b00000000;
-					Motor_Moving_Forward();
-				}
-				stage3CntTurnLeft++;
-				stage3CntMoveForward++;
-				if(stage3CntTurnLeft >= 3 && stage3CntMoveForward >= 3){
-					systemMode = 1; // systemMode를 Map2에 알맞게 설정하기.
-				}
-			}
 		}
 	}else if(systemMode == 1){ // Map2에서의 systemMode
-		PORTA = 0b11111111;
 		
-		if(psdSnesorList[1] < 250){
-			PORTA = 0b11110000;
-			Motor_Moving_Forward();
-			Motor_Turning_Right();
-			if(irSensorList[5] > 50){
-				Motor_Turning_Right();
-			}else if(irSensorList[5] < 50){
-				Motor_Turning_Left();
-			}
-		}else if(psdSnesorList[1] > 250){
-			PORTA = 0b00001111;
-			Motor_Moving_Forward();
-			Motor_Turning_Left();
-			if(irSensorList[5] > 50){
-				Motor_Turning_Right();
-			}else if(irSensorList[5] < 50){
-				Motor_Turning_Left();
-			}
-		}
 	}
-	
-	
 }
 
 // IR Sensor Normalization Value 찾기 모드.
